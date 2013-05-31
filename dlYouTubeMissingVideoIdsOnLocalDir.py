ac7926cbb2b0b285911099f612bebd822b6bcf4a
Luiz Plus <livrosetc@yahoo.com.br>
2013-05-30 22:13:59 -0300

    modified:   dlYouTubeMissingVideoIdsOnLocalDir.py

--- dlYouTubeMissingVideoIdsOnLocalDir.py
+++ dlYouTubeMissingVideoIdsOnLocalDir.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 '''
 Explanation: 
-  This script reads a .txt file that have YouTube filenames as given by youtube-dl
-  It can also read filenames that have starts with '-' (dash) before the videoid.
+  This script reads a .txt file, which has a hardcoded name z-filenames.txt
+  that have YouTube filenames as given by youtube-dl
+  (this filename is the video title, a '-' (dash), its videoid, then the dot extension)
+  It can also read filenames that have starts with '-' (dash) before the videoid, ie,
+  filenames that do not have the title part.
   
 
   Limitations:
-    A safe rule to check whether an id is a videoid or not, that has not been implemented.
-    For the time being, videoids have to contain 11 characters and not have '%' nor '.'
+  
+    A safe rule to check whether an id is a valid videoid or not, that has not been implemented.
+    For the time being, videoids have the following checking (in function return_videoid_if_good_or_None(videoid)):
+    + it must contain 11 characters;
+    + it must not have the following characters: ' ' (blank), '%' nor '.';
+    + it must have at least one lowercase letter and one UPPERCASE letter.
 
 '''
 import glob, logging, os, sys, time
@@ -18,6 +25,7 @@ def print_and_log(line_msg):
   logging.info(line_msg)
 
 BASE_COMMAND_INDIVUAL_VIDEO = 'youtube-dl -w -f 18 http://www.youtube.com/watch?v=%(videoid)s'
+TWO_MIN_IN_SECS = 2 * 60
 
 def download_individual_video(videoid, p_seq=1, total_to_go=1):
   retVal = -1; loop_seq = 0
@@ -58,39 +66,51 @@ def return_videoid_if_good_or_None(videoid):
     return None
   elif videoid.find('%') > -1:
     return None
-  filter_result = map(has_lowercase_lambda, videoid)
-  if True not in filter_result:
+  boolean_list_result = map(has_lowercase_lambda, videoid)
+  if True not in boolean_list_result:
     return None
-  filter_result = map(has_UPPERCASE_lambda, videoid)
-  if True not in filter_result:
+  boolean_list_result = map(has_UPPERCASE_lambda, videoid)
+  if True not in boolean_list_result:
     return None
   return videoid
 
-def get_videoids_on_file(local_filename):
-  videoids_on_file = []
-  lines = open(local_filename).readlines()
-  for line in lines:
-    if line.endswith('\n'):
-      line = line.rstrip('\n')
-    if line.find('.') < 0:
-      continue
-    # strip extension and recompose filename without extension
-    try:
-      extlessname = '.'.join(line.split('.')[:-1])
-      videoid = get_videoid(extlessname)
-      videoid = return_videoid_if_good_or_None(videoid)
-      if videoid == None:
+class VideoIdsOnFile(object):
+
+  def __init__(self, local_filename):
+    self.local_filename = local_filename
+    self.find_videoids_on_file()
+
+  def find_videoids_on_file(self):
+    self.videoids_on_file = []
+    lines = open(self.local_filename).readlines()
+    for line in lines:
+      if line.endswith('\n'):
+        line = line.rstrip('\n')
+      if line.find('.') < 0:
         continue
-      videoids_on_file.append(videoid)
-    except IndexError:
-      continue
-  return videoids_on_file
+      # strip extension and recompose filename without extension
+      try:
+        extlessname = '.'.join(line.split('.')[:-1])
+        videoid = get_videoid(extlessname)
+        videoid = return_videoid_if_good_or_None(videoid)
+        if videoid == None:
+          continue
+        if videoid not in self.videoids_on_file:
+          self.videoids_on_file.append(videoid)
+      except IndexError:
+        continue
+
+  def get_videoids_on_file(self):
+    return self.videoids_on_file
 
 def checkEveryFileHas11CharId():
   mp4s = glob.glob('*.mp4'); seq=0
   for mp4 in mp4s:
-    extlessname = os.path.splitext(mp4)[0]
-    prefixedId = extlessname[-12:]
+    try:
+      extlessname = os.path.splitext(mp4)[0]
+      prefixedId = extlessname[-12:]
+    except IndexError:
+      continue
     if prefixedId.startswith('-'): 
       seq += 1
     print seq, prefixedId
@@ -116,12 +136,12 @@ class VideoIdsComparer(object):
         continue
 
   def compareLocalIdsWithFileDB(self):
-    self.missing_videoids = []
-    videoids = get_videoids_on_file(self.local_filename); seq = 0
-    for videoid in videoids:
+    self.missing_videoids = []; n_missing = 0
+    videoidsObj = VideoIdsOnFile(self.local_filename)
+    for videoid in videoidsObj.get_videoids_on_file():
       if videoid not in self.all_videoids:
-        seq += 1
-        # print seq, 'VideoId', videoid, 'in file not on local dir.'
+        n_missing += 1
+        # print n_missing, 'VideoId', videoid, 'in file not on local dir.'
         self.missing_videoids.append(videoid)
 
   def download_missing_videos(self):
@@ -149,4 +169,3 @@ def process():
   
 if __name__ == '__main__':
   process()
-
