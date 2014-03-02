26ec82716f44f6d417a8edc8cc9152403c124d9c
Luiz Lewis <livrosetc@yahoo.com.br>
2014-03-02 16:20:14 -0300

    Created renameConservingVideoid.py et al.

---
+++ dlYouTubeWithIdsOnTxtFile3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+
+'''
+import sys
+sys.path.insert(0, '/home/dados/Sw3/SwDv/CompLang SwDv/PythonSwDv/PythonScrapingEtcSwDv/uTubeOurApps/')
+from shellclients.dlYouTubeWithIdsOnTxtFile import VideoidsGrabberAndDownloader 
+
+def process():
+  youtubeids_filename = None
+  try:
+    youtubeids_filename = sys.argv[1]
+  except IndexError:
+    pass
+  VideoidsGrabberAndDownloader(youtubeids_filename)  
+
+if __name__ == '__main__':
+  process()
--- renameAddingExtension.py
+++ renameAddingExtension.py
@@ -1,11 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import glob, os
+import glob, os, sys
 
-files = glob.glob('*')
-for i in range(1, 37):
-  iStr = '%02d' %i
-  mp3 = glob.glob('%s*' %iStr)[0]
-  if not mp3.endswith('.mp3'):
-    os.rename(mp3, mp3 + '.mp3')
+
+class Renamer(object):
   
+  def __init__(self, extension=None):
+    if extension == None:
+      raise ValueError, 'Extension should be entered.'
+    self.extension = extension
+    self.rename_pairs = []
+    self.process()
+
+  def process(self):
+    self.search_renames()
+    if self.confirm_renames():
+      self.do_renames()
+    
+  def search_renames(self):
+    files = glob.glob('*')
+    for old_name in files:
+      new_name = old_name + '.%s' %self.extension
+      rename_pair = (old_name, new_name)
+      self.rename_pairs.append(rename_pair)
+
+  def confirm_renames(self):
+    for i, rename_pair in enumerate(self.rename_pairs):
+      seq = i + 1
+      print seq, 'Rename:'
+      old_name, new_name = rename_pair
+      print 'FROM: >>>%s' %old_name
+      print 'TO:   >>>%s' %new_name
+    ans = raw_input('Confirm the %d renames above ? (y/N) ' %len(self.rename_pairs))
+    if ans.lower() == 'y':
+      return True
+    print 'No renames were done.'
+    return False
+    
+  def do_renames(self):
+    print '-'*40
+    for i, rename_pair in enumerate(self.rename_pairs):
+      old_name, new_name = rename_pair
+      seq = i + 1
+      print seq, 'Rename:'
+      print 'FROM: >>>%s' %old_name
+      print 'TO:   >>>%s' %new_name
+      os.rename(old_name, new_name)
+    print '-'*40
+    print 'Finished %d renames.' %len(self.rename_pairs)
+    
+
+def process():
+  '''
+  '''
+  Renamer(sys.argv[1])
+
+if __name__ == '__main__':
+  process()
