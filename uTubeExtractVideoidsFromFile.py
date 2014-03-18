f8095792b97e5c9661ed5dee948d71ba3f71250f
Luiz Lewis <livrosetc@yahoo.com.br>
2014-03-18 18:28:24 -0300

    Added uTubeExtractVideoidsFromFile.py

---
+++ uTubeExtractVideoidsFromFile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import os, sys, time
+
+import __init__
+import  bin_local_settings as bls
+
+sys.path.insert(0, bls.UTUBEAPP_PATH)
+
+from shellclients import extractVideoidsFromATextFileMod as extract_script
+
+def process():
+  extract_script.process()
+
+if __name__ == '__main__':
+  process()
