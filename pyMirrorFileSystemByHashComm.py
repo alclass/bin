e1309ac722d909913eecdd463731a3398634cc20
Luiz Lewis <livrosetc@yahoo.com.br>
2016-01-18 03:05:21 -0200

    Minor update in pyMirrorFileSystemByHashComm.py to pick it up on another machine

--- pyMirrorFileSystemByHashComm.py
+++ pyMirrorFileSystemByHashComm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-batchWalkHashingFilesToRootFolderSqliteDB.py
+pyMirrorFileSystemByHashComm.py (former batchWalkHashingFilesToRootFolderSqliteDB.py)
 Explanation:
   This script walks all files and folders up the directory tree.
   As it encounters files it sha1-hashes them and store sha1hex on a database.
@@ -29,35 +29,52 @@ import  bin_local_settings as bls
 sys.path.insert(0, bls.PyMirrorFileSystemsByHash_PATH) # the PATH to this lib is placed (configured) in bin_local_settings.py
 import Sha1FileSystemComplementer as fsComplementorMod
 
-def walk_on_up_tree_to_grab_sha1hex():
+def walk_on_up_tree_to_grab_sha1hex(DEVICE_ROOT_ABSPATH, FURTHER_ABOVE_ON_DEVICE_PATH=None):
   '''
 
   :return:
   '''
-  global device_root_abspath
-  device_root_abspath = os.path.abspath('.')
-  fs_complementer = fsComplementorMod.Sha1FileSystemComplementer(device_root_abspath)
+  if FURTHER_ABOVE_ON_DEVICE_PATH == None:
+    start_abspath = os.path.abspath('.')
+  else:
+    start_abspath = os.path.abspath(FURTHER_ABOVE_ON_DEVICE_PATH)
+  if not start_abspath.startswith(DEVICE_ROOT_ABSPATH):
+    raise Exception, 'Inconsistency: the device root path is not beginning the starting path for dirwalk, the point from the mirroring traversal will begin.'
+  fs_complementer = fsComplementorMod.Sha1FileSystemComplementer(DEVICE_ROOT_ABSPATH) # instantiation uses the DEVICE_ROOT_ABSPATH not the start_abspath
   walk_counter = 0
-  for dirpath, dirnames, filenames in os.walk(device_root_abspath):
+  for dirpath, dirnames, filenames in os.walk(start_abspath):
     fs_complementer.db_insert_dirnames(dirnames, dirpath)
-    complement_path = dirpath
-    if complement_path.startswith('./'):
-      complement_path = complement_path[2:]
-    current_abs_dirpath = os.path.join(device_root_abspath, complement_path)
     walk_counter += 1
-    print walk_counter, 'current path:', current_abs_dirpath
-    print 'Files found @', complement_path
-    for filename in filenames:
-      print filename
-    os.chdir(current_abs_dirpath)
-    fs_complementer.(current_abs_dirpath, filenames)
-    os.chdir(device_root_abspath)
-    print '-'*40
-    print 'Voltei!', time.ctime(), dirpath, dirnames, current_abs_dirpath
+    print walk_counter, 'current path:', dirpath
+    n_of_files = len(filenames)
+    if n_of_files > 0:
+      print 'Files found:'
+      for filename in filenames:
+        print filename
+      print 'Total of files :', n_of_files
+    n_of_dirs = len(dirnames)
+    if n_of_dirs > 0:
+      print 'Folders found:'
+      for dirname in dirnames:
+        print dirname
+      print 'Total of folders :', n_of_dirs
+    # insert or update the sha1hex on the sqlite-db on the device's root folder
+    fs_complementer.p(current_abs_dirpath, filenames)
+    print time.ctime(), dirpath, dirnames
     print '='*40
 
+def get_args():
+  DEVICE_ROOT_ABSPATH = None
+  for arg in sys.argv:
+    if arg.startswith('-d='):
+      given_device_root_abspath = arg[ len('-d=') : ]
+      if os.path.isdir(given_device_root_abspath):
+        DEVICE_ROOT_ABSPATH = given_device_root_abspath
+  return DEVICE_ROOT_ABSPATH
+
 def main():
-  walk_on_up_tree_to_grab_sha1hex()
+  DEVICE_ROOT_ABSPATH = get_args()
+  walk_on_up_tree_to_grab_sha1hex(DEVICE_ROOT_ABSPATH)
 
 if __name__ == '__main__':
   # main()
