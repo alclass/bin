9583940c97a15d9c92f465f07a619bf576eb65ec
Luiz Lewis <livrosetc@yahoo.com.br>
2019-08-24 21:37:34 -0300

    Updated readTheDocsUrlCompleter.py

--- readTheDocsUrlCompleter.py
+++ readTheDocsUrlCompleter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
 readTheDocsUrlCompleter.py
@@ -7,40 +7,117 @@ readTheDocsUrlCompleter.py
 import os, sys
 
 URL_BASE = 'http://media.readthedocs.org/%(format_id)s/%(project_name)s/latest/%(project_name)s.%(file_extension)s'
+# https://buildmedia.readthedocs.org/media/%(format_id)s/%(project_name)s/latest/%(project_name)s.%(file_extension)s
+# https://readthedocs.org/projects/tags/%(tagname)s/
+
+DEFAULT_FILE_EXT_LIST = ['epub']
+
+class RtdocsDownload:
+  '''
+
+  '''
+
+  def __init__(self, project_names, file_ext_list):
+    '''
+
+    :param project_names:
+    :param file_ext_list:
+    '''
+    self.n_downloads = 0
+    self.do_download = True # prove otherwise
+    self.set_project_names(project_names)
+    self.set_file_ext_list(file_ext_list)
+    self.process_download()
+
+  def set_project_names(self, project_names):
+    '''
+
+    :param project_names:
+    :return:
+    '''
+    if type(project_names) != list or project_names == []:
+      error_msg = 'Project names list either does not exist or is empty.'
+      raise ValueError(error_msg)
+
+    self.project_names = list(project_names)
+
+  def set_file_ext_list(self, file_ext_list):
+    '''
+
+    :param file_ext_list:
+    :return:
+    '''
+    if type(file_ext_list) != list or file_ext_list == []:
+      self.file_ext_list = list(DEFAULT_FILE_EXT_LIST)
+      return
+    self.file_ext_list = list(file_ext_list)
+
+  def process_download(self):
+    '''
+
+    :return:
+    '''
+    self.list_downloads_from_readthedocs_for_confirmation()
+    if self.do_download:
+      self.download_from_readthedocs()
+
+  def list_downloads_from_readthedocs_for_confirmation(self):
+    '''
+
+    :return:
+    '''
+    n_total = 0
+    for i, project_name in enumerate(self.project_names):
+      n_project = i + 1
+      for file_extension in self.file_ext_list: # ['pdf', 'zip', 'epub']
+        n_total += 1
+        print ( n_project, n_total, project_name, file_extension )
+    print ('='*40)
+    print ('Please, confirm dowloads above:')
+    print ('='*40)
+    ans = input('Download? (Y/n) ')
+    if ans in ['N', 'n']:
+      self.do_download = False
+
+  def download_from_readthedocs(self):
+    '''
+
+    :return:
+    '''
+
+    n_downloads = 0
+    if not self.do_download:
+      return
+    for i, project_name in enumerate(self.project_names):
+      i_project = i + 1
+      for file_extension in self.file_ext_list: # ['pdf', 'zip', 'epub']
+        n_downloads += 1
+        print ( i_project, n_downloads, project_name, file_extension )
+        self.go_download(n_downloads, project_name, file_extension)
+
+  def go_download(self, n_downloads, project_name, file_extension):
+    '''
+
+    :param project_name:
+    :param file_extension:
+    :return:
+    '''
+
+    format_id = get_format_id_from_file_extension(file_extension)
+    urldict = {
+      'format_id' : format_id,
+      'project_name' : project_name,
+      'file_extension' :file_extension,
+    }
+    url = URL_BASE %urldict
+    comm = 'wget -c %s' %url
+    print ( n_downloads, comm )
+    os.system(comm)
 
 def get_format_id_from_file_extension(file_extension):
   if file_extension == 'zip':
     return 'htmlzip'
   return file_extension
-
-n_downloads = 0
-def go_download(project_name, file_extension):
-  global n_downloads
-
-  format_id = get_format_id_from_file_extension(file_extension)
-  urldict = {'format_id':format_id, 'project_name':project_name,'file_extension':file_extension}
-  url = URL_BASE %urldict
-  comm = 'wget -c %s' %url
-  n_downloads += 1
-  print n_downloads, comm
-  os.system(comm)
-
-def download_from_readthedocs(project_names, file_ext_list=['epub'], do_download=False):
-  n_project = 0; n_total = 0
-  for i, project_name in enumerate(project_names):
-    n_project = i + 1
-    for file_extension in file_ext_list: # ['pdf', 'zip', 'epub']
-      n_total += 1
-      print n_project, n_total, project_name, file_extension
-      if do_download:
-        go_download(project_name, file_extension)
-  if do_download:
-    return
-  print '='*40
-  print 'Please, confirm dowloads above:'
-  print '='*40
-  ans = raw_input('ok? (if not, break execution with Ctrl-C) ')
-  download_from_readthedocs(project_names, file_ext_list, do_download=True)
 
 def extract_file_ext_list(arg):
   file_ext_list_str = ''
@@ -56,14 +133,17 @@ def extract_file_ext_list(arg):
 
 def get_args():
   project_names = []
-  file_ext_list = ['epub']
+  file_ext_list = list(DEFAULT_FILE_EXT_LIST)
   for arg in sys.argv[1:]:
     if arg.startswith('-el='):
       file_ext_list = extract_file_ext_list(arg)
-    project_names.append(arg)
+    else:
+      project_names.append(arg)
   return project_names, file_ext_list
 
-if __name__ == '__main__':
+def process():
   project_names, file_ext_list = get_args()
-  download_from_readthedocs(project_names, file_ext_list)
+  RtdocsDownload(project_names, file_ext_list)
 
+if __name__ == '__main__':
+  process()
