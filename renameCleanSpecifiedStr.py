a8662bd195f7c7211dde61c94165bf592debf283
Luiz Lewis <livrosetc@yahoo.com.br>
2020-03-08 22:50:57 -0300

    Updating renameCleanSpecifiedStr.py & renameSubtitleNameToVideoName.py

--- renameCleanSpecifiedStr.py
+++ renameCleanSpecifiedStr.py
@@ -1,18 +1,142 @@
+#!/usr/bin/env python3
+'''
+This script extracts a specified string from all files having a chosen extension.
+
+For the time being it only works with the mp4 extension
+  TO-DO:  parameter -e=<extension> will be introduced later on and the script will work with any extension.
+
+Example:
+
+=> Suppose there are the 3 following files on the target folder (target folder is the current directory at this version):
+    [file1 blah.mp4] [file2 blah.mp4] [file3 blah.mp4]
+
+=> Running the script as:
+
+          prompt$ renameCleanSpecifiedStr.py -s=" blah"
+
+will rename them to (with confirmation (y/n) or without it [not yet implemented, TO-DO with parameter -y]):
+    [file1.mp4] [file2.mp4] [file3.mp4]
+
+Ie, the string " blah" will be extracted away from each mp4 file on folder.
+'''
 import glob, os, sys
 
-sys.exit(1)
 
-det_str = '   YouTube 360p'
-files = glob.glob('*.mp4')
-n_renames = 0
-for eachFile in files:
-  if eachFile.find(det_str):
-    newname = eachFile.replace(det_str, '')
-    newname = newname.replace('  ', ' ')
-    if os.path.isfile(newname):
+DEFAULT_RENAME_EXTENSION = 'mp4'
+
+class Renamer:
+  '''
+  class Renamer:
+  '''
+  def __init__(self, specified_str, no_confirm=False, extension=None, abspath=None):
+    '''
+
+    :param specified_str:
+    :param extension:
+    :param abspath:
+    '''
+    self.specified_str = specified_str
+    self.extension     = extension
+    if self.extension is None:
+      self.extension = DEFAULT_RENAME_EXTENSION
+    self.abspath = abspath
+    if self.abspath is None or not os.path.isdir(self.abspath):
+      self.abspath = os.path.abspath('.')
+    self.target_filenames    = []; self.n_target_filenames = 0
+    self.rename_pairs = [];        self.n_renames = 0
+    self.no_confirm = no_confirm
+    self.confirm_rename = False
+    self.rename_process()
+
+  def rename_process(self):
+    '''
+
+    :return:
+    '''
+    os.chdir(self.abspath)
+    self.target_filenames = glob.glob('*.' + self.extension)
+    self.confirm_rename = False
+    self.prepare_for_rename()
+    if self.confirm_rename:
+      self.do_rename()
+    self.show_numbers()
+
+  def prepare_for_rename(self):
+    '''
+
+    :return:
+    '''
+    print ("Folder =>", self.abspath)
+    print ("Replace str =>", self.specified_str)
+    for i, target_filename in enumerate(self.target_filenames):
+      if target_filename.find(self.specified_str):
+        newname = target_filename.replace(self.specified_str, '')
+        if os.path.isfile(newname):
+          continue
+        rename_pair = (target_filename, newname)
+        self.rename_pairs.append(rename_pair)
+        seq = i + 1
+        print(seq, ' => Rename:')
+        print('FROM:', target_filename)
+        print('TO:  ', newname)
+
+    if self.no_confirm:
+      self.confirm_rename = True
+      return
+
+    if len(self.rename_pairs) == 0:
+      self.confirm_rename = False
+      return
+
+    ans = input('Rename them (y/N) ? ')
+    # self.confirm_rename = False
+    if ans in ['y', 'Y']:
+      self.confirm_rename = True
+
+  def do_rename(self):
+    '''
+
+    :return:
+    '''
+    self.n_renames = 0
+    for i, rename_pair in enumerate(self.rename_pairs):
+      target_filename = rename_pair[0]
+      new_namefile  = rename_pair[1]
+      if not os.path.isfile(target_filename):
         continue
-    print ('FROM:', eachFile)
-    print ('TO:', newname)
-    os.rename(eachFile, newname)
-    n_renames += 1
-print('n_renames', n_renames)
+      if os.path.isfile(new_namefile):
+        continue
+      self.n_renames += 1
+      print('FROM =>', target_filename)
+      print('TO   =>', new_namefile)
+      os.rename(target_filename, new_namefile)
+
+  def show_numbers(self):
+    '''
+
+    :return:
+    '''
+    print ('Number of rename pairs:', len(self.rename_pairs))
+    print ('Number of renamed:',      self.n_renames)
+
+def get_str_arg():
+  specified_str = None
+  for arg in sys.argv:
+    if arg.startswith('--help'):
+      print(__doc__)
+      sys.exit(0)
+    elif arg.startswith('-s='):
+      specified_str = arg[ len('-s=') : ]
+      break
+  if specified_str is None:
+    print (__doc__)
+    error_msg = 'specified_str is missing. Program cannot continue.'
+    raise ValueError(error_msg)
+  return specified_str
+
+def process():
+  specified_str = get_str_arg()
+  Renamer(specified_str)
+
+if __name__ == '__main__':
+  process()
--- renameSubtitleNameToVideoName.py
+++ renameSubtitleNameToVideoName.py
@@ -1,30 +1,143 @@
+#!/usr/bin/env python3
 import glob, os
-SUBTITLE_EXT = '.srt'
-VIDEO_EXT = '.mp4'
 
-n_renames = 0
-mp4s = glob.glob('*'+VIDEO_EXT)
-sorted( mp4s )
+DEFAULT_SUBTITLE_EXT = '.srt'
+DEFAULT_VIDEOFILE_EXT = '.mp4'
 
-subs = glob.glob('*'+SUBTITLE_EXT)
-sorted( subs )
+class Rename:
+  '''
 
-if len(mp4s) != len(subs):
-  raise Except('len(mp4s) != len(subs)')
+  '''
+  def __init__(self, workfolder_absdir = None, no_confirm = False):
+    self.videofile_ext = None
+    self.subtitle_ext  = None
+    self.videofilenames    = []; self.n_filenames = 0
+    self.subtitlefilenames = []; self.n_subtitles = 0
+    self.rename_pairs      = []; self.n_renames = 0
+    self.workfolder_absdir = None
+    self.set_workfolder_absdir(workfolder_absdir)
+    self.no_confirm = no_confirm
+    self.confirm_rename = False
+    self.rename_process()
 
-print ('oi')
+  def set_workfolder_absdir(self, workfolder_absdir):
+    if self.workfolder_absdir is None:
+      self.workfolder_absdir = os.path.
 
-for i, videofilename in enumerate(mp4s):
-  videoname, _ = os.path.splitext(videofilename)
-  subfilename = subs[i]
-  _, subext = os.path.splitext(subfilename)
-  subNewname = videoname + subext
-  if os.path.isfile(subNewname):
-    continue
-  print ('FROM ', videofilename )
-  print ('FROM ', subfilename )
-  print ('TO ', subNewname )
-  os.rename(subfilename, subNewname)
-  n_renames += 1
 
-print ('n_renames', n_renames)
+
+
+  def rename_process(self):
+    '''
+
+    :return:
+    '''
+    self.verify_defaults()
+    self.load_video_n_subs_lists()
+    self.prepare_for_rename()
+    if self.confirm_rename:
+      self.do_rename()
+    self.show_numbers()
+
+  def verify_defaults(self):
+    '''
+
+    :return:
+    '''
+    if self.videofile_ext is None:
+      self.videofile_ext = DEFAULT_VIDEOFILE_EXT
+    if self.subtitle_ext is None:
+      self.subtitle_ext = DEFAULT_SUBTITLE_EXT
+
+  def load_video_n_subs_lists(self):
+
+    self.videofilenames = glob.glob('*' + self.videofile_ext)
+    self.videofilenames = sorted( self.videofilenames )
+
+    self.subtitlefilenames = glob.glob('*' + self.subtitle_ext)
+    self.subtitlefilenames = sorted( self.subtitlefilenames )
+
+    self.n_filenames = self.videofilenames
+    self.n_subtitles = self.subtitlefilenames
+    if len(self.n_filenames) != len(self.n_subtitles):
+      error_msg = 'Number of videofiles (%d) is different than number of subtitles (%d)' %(self.n_filenames, self.n_subtitles)
+      raise Except(error_msg)
+
+  def prepare_for_rename(self):
+    '''
+
+    :return:
+    '''
+
+    for i, videofilename in enumerate(self.videofilenames):
+      if not os.path.isfile(videofilename):
+        continue
+      videoname, _ = os.path.splitext(videofilename)
+      subfilename = self.subtitlefilenames[i]
+      _, subext = os.path.splitext(subfilename)
+      subNewnamefile = videoname + subext
+      if os.path.isfile(subNewnamefile):
+        continue
+      seq = i + 1
+      print (seq, ' => Rename:')
+      print ('FROM =>', subfilename )
+      print ('BASE =>', videofilename )
+      print ('TO   =>', subNewnamefile )
+      rename_pair = (subfilename, subNewnamefile)
+      self.rename_pairs.append(rename_pair)
+
+    print ('n_rename_pairs', len(self.rename_pairs))
+
+    if self.no_confirm:
+      self.confirm_rename = True
+
+    ans = input('Rename them (y/N) ? ')
+    self.confirm_rename = False
+    if ans in ['y', 'Y']:
+      self.confirm_rename = True
+
+  def do_rename(self):
+    '''
+
+    :return:
+    '''
+    self.n_renames = 0
+    for i, rename_pair in enumerate(self.rename_pairs):
+      subfilename    = rename_pair[0]
+      subNewnamefile = rename_pair[1]
+      if not os.path.isfile(subfilename):
+        continue
+      if os.path.isfile(subNewnamefile):
+        continue
+      seq = i + 1
+      print (seq, ' => Renaming now:')
+      print ('FROM ', subfilename)
+      print ('TO   ', subNewnamefile)
+      os.rename(subfilename, subNewnamefile)
+      self.n_renames += 1
+
+  def show_numbers(self):
+    '''
+
+    :return:
+    '''
+    print ('Number of rename pairs:', len(self.rename_pairs))
+    print ('Number of renamed:', self.n_renames)
+
+def walkup_folders():
+  entries = os.listdir('.')
+  for entry in entries:
+    if os.path.isdir(entry):
+      absdir = os.path.absdir('.')
+      workfolder_absdir = os.path.join(absdir, entry)
+
+
+
+
+
+
+def process():
+  Rename()
+
+if __name__ == '__main__':
+  process()
