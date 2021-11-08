37e7b549e33e0cde3c0969ce2e3d685b0949be2a
Luiz Lewis <livrosetc@yahoo.com.br>
2021-11-08 12:48:19 -0300

    Updated renameReplace12.py (can now rename without confirmation and rename thru folder and subdirectories)

--- renameReplace12.py
+++ renameReplace12.py
@@ -2,11 +2,26 @@
 """
 This script replaces a given string (s1) to another one (s2) in filenames of a certain extension.
 
-Usage: <this_script> -e="<ext>" -s1="<string1>" -s2="<string2>"
-Example: renameReplace12 -e="mp4" -s1="foo" -s2="bar"
+Usage: <this_script> -e="<ext>" -s1="<string1>" -s2="<string2>" [-y] [-dw]
 
+Parameters:
+  -e="<ext>" :: example: mp4, webm etc
+  -s1="<string1>" :: example: "file"
+   -s2="<string2>" :: example: "ficheiro" (ie substitute word ficheiro for the ocorrências of word file)
+  -y :: noconfirm ie include -y to command for the Y/n confirmation (it will rename without the confirmation step)
+  -dw :: dodirwalk ie include -dw for renaning "up dir tree"
+         ie the script will apply the rename command to subdirectories
+
+Examples:
+  1) $renameReplace12 -e="mp4" -s1="foo" -s2="bar"
 In the example above, any file containing the string "foo" in its name will have it changed to string "bar".
 Suppose there is a filename "foo bar.mp4". After the example above, its name will become "bar bar.mp4"
+
+  2) $renameReplace12 -e="mp4" -s1="foo" -s2="bar" -y
+Same thing as above without the confirmation step.
+
+  3) $renameReplace12 -e="mp4" -s1="foo" -s2="bar" -y -dw
+Same thing as above also renaming files inside subdirectories.
 """
 import glob
 import os
@@ -20,11 +35,12 @@ def fetch_files_on_folder(ext):
 
 class Renamer:
 
-  def __init__(self, ext, piece1, piece2):
+  def __init__(self, ext, piece1, piece2, noconfirm=False):
     self.rename_pairs = []
     self.ext = ext
     self.piece1 = piece1
     self.piece2 = piece2
+    self.noconfirm = noconfirm
     self.files = fetch_files_on_folder(self.ext)
 
   def prepare_rename(self):
@@ -72,12 +88,16 @@ class Renamer:
 
   def process_rename(self):
     self.prepare_rename()
-    if self.confirm_rename():
+    if self.noconfirm:
       self.do_rename()
+    elif self.confirm_rename():
+      self.do_rename()
+    else:
+      print('No files renamed.')
 
 
 def get_args():
-  args = {'ext': None, 'piece1': '', 'piece2': ''}
+  args = {'ext': None, 'piece1': '', 'piece2': '', 'noconfirm': True, 'dodirwalk': False}
   for arg in sys.argv:
     if arg.startswith('-h') or arg.startswith('--help'):
       print(__doc__)
@@ -91,15 +111,51 @@ def get_args():
     elif arg.startswith('-s2='):
       piece2 = arg[len('-s2='):]
       args['piece2'] = piece2
+    elif arg.startswith('-y'):
+      args['noconfirm'] = True
+    elif arg.startswith('-dw'):
+      args['dodirwalk'] = True
   return args
+
+
+def issue_renamer_on_current_folder(ext, piece1, piece2, noconfirm):
+  ren = Renamer(ext, piece1, piece2, noconfirm)
+  ren.process_rename()
+
+
+def process_dir_walk(ext, piece1, piece2, noconfirm):
+  base_absdir = os.path.abspath('.')
+  n_dirwalks = 0
+  for current_folder, dirs, files in os.walk(base_absdir):
+    n_dirwalks += 1
+    print(n_dirwalks, 'Current folder:', current_folder)
+    current_dirpath = os.path.join(base_absdir, current_folder)
+    os.chdir(current_dirpath)
+    issue_renamer_on_current_folder(ext, piece1, piece2, noconfirm)
+  print('n_dirwalks =', n_dirwalks)
+
+
+def adhoc_test():
+  base_absdir = os.path.abspath('.')
+  n_dirwalks = 0
+  for current_folder, dirs, files in os.walk(base_absdir):
+    n_dirwalks += 1
+    print(n_dirwalks, 'Current folder:', current_folder)
+    current_dirpath = os.path.join(base_absdir, current_folder)
+    os.chdir(current_dirpath)
+  print('n_dirwalks =', n_dirwalks)
 
 
 def process():
   args = get_args()
-  ext, piece1, piece2 = args['ext'], args['piece1'], args['piece2']
-  ren = Renamer(ext, piece1, piece2)
-  ren.process_rename()
+  ext, piece1, piece2, noconfirm, dodirwalk = \
+    args['ext'], args['piece1'], args['piece2'], args['noconfirm'], args['dodirwalk']
+  if dodirwalk:
+    process_dir_walk(ext, piece1, piece2, noconfirm)
+  else:
+    issue_renamer_on_current_folder(ext, piece1, piece2, noconfirm)
 
 
 if __name__ == '__main__':
+  # adhoc_test()
   process()
