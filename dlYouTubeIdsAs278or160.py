c474a8dd34b920abee346fdfbb4b9deb2f7d5045
Luiz Lewis <livrosetc@yahoo.com.br>
2022-10-31 15:34:00 -0300

    Improving dlYouTubeIdsAs278or160.py

--- dlYouTubeIdsAs278or160.py
+++ dlYouTubeIdsAs278or160.py
@@ -1,43 +1,95 @@
 #!/usr/bin/env python3
 """
 dlYouTubeIdsAs278or160.py
-This script read a file with youtube-ids and tries to download one by one.
-1) The first try issues a 278+249 download attempt (ie a webm video+audio composite)
-2) if 'format not available' is detected, a second try issue a 160+140 download attempt (mp4)
-3) if 'failure in name resolution' is detected, it waits a little and retries back to 1.
+This script reads a text file with youtube-ids and
+  tries to download one by one. Three formats are tried, in the following order:
+
+1) The first download try issues a 278+249 format attempt
+   (ie a webm video+audio composite)
+2) if the above fails (ie, if 'format not available' is detected),
+   the script falls back to a second download try and
+   issues a 160+139 format attempt
+    (ie a mp4 video+audio composite)
+3) if the above fails (ie, if 'format not available' is detected)
+   a third download try issues a 160+140 download attempt (mp4)
+    (ie a mp4 video+audio observing that audio 140 is larger than 139)
+4) if none of the above downloads, it prints out a message informing
+   'format <if-any> not recognized.
+In a nutshell, the following formats are tried in this order:
+  V160_139 = 160 video + 139 audio
+  V160_140 = 160 video + 140 audio
+  V278_249 = 278 video + 249 audio
+TO-DO:
+  The script might find out all available formats and
+  issue the first one coinciding in a priority queue
+  * in this strategy, no fall-back would happend for
+    in this case the available ones are known.
+  This can be achieved by $youtube-dl -F <ytid>
+    which lists all available formats.
 """
-import os
 from subprocess import PIPE, Popen
 import string
-import time
 
 
 DEFAULT_YTIDS_FILENAME = 'youtube-ids.txt'
 YTID_CHARSIZE = 11
 FORMAT_NOT_AVAILABLE_MSG = 'requested format not available'
 FAILURE_IN_NAME_RESO_MSG = 'failure in name resolution'
-basecomm160 = 'youtube-dl -w -f 160+140 {ytid}'
-basecomm278 = 'youtube-dl -w -f 278+249 {ytid}'
 ENC64 = string.ascii_lowercase + string.ascii_uppercase + string.digits + '_' + '-'
 WAIT_TIME_IN_SEC = 30
 
 
-def download_ytid(ytid):
-  comm = basecomm278.format(ytid=ytid)
-  print(comm)
-  p = Popen(comm, shell=True, stdout=PIPE, stderr=PIPE)
-  stdout, stderr = p.communicate()
-  res = str(stderr)
-  print('OS command response: [[', res, ']]')
-  if FORMAT_NOT_AVAILABLE_MSG.lower() in res.lower():
-    print(FORMAT_NOT_AVAILABLE_MSG, ':: Trying format 160+140')
-    comm = basecomm160.format(ytid=ytid)
-    print('Retrying with [', comm, ']')
-    os.system(comm)
-  elif FAILURE_IN_NAME_RESO_MSG.lower() in res.lower():
-    print(FAILURE_IN_NAME_RESO_MSG, ':: Wait a little before retrying (', WAIT_TIME_IN_SEC, 'sec)')
-    time.sleep(WAIT_TIME_IN_SEC)
-    return download_ytid(ytid)
+class VType:
+  def __init__(self):
+    pass
+  V160_139 = '160+139'
+  basecomm160_139 = 'youtube-dl -w -f 160+139 {ytid}'
+  V160_140 = '160+140'
+  basecomm160_140 = 'youtube-dl -w -f 160+140 {ytid}'
+  V278_249 = '278+249'
+  basecomm278_249 = 'youtube-dl -w -f 278+249 {ytid}'
+
+
+def download_ytid(ytid, videotype=None, formats_tried=()):
+  formats_tried = tuple(list(formats_tried) + [videotype])
+  if videotype is None or videotype == VType.V278_249:
+    comm = VType.basecomm278_249.format(ytid=ytid)
+    print(comm)
+    p = Popen(comm, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    res = str(stderr)
+    print('OS command response stderr: [[', res, ']]')
+    if FORMAT_NOT_AVAILABLE_MSG.lower() in res.lower():
+      print(FORMAT_NOT_AVAILABLE_MSG, ':: Trying format', VType.V160_139)
+      return download_ytid(ytid, VType.V160_139)
+    res = str(stdout)
+    print('OS command response stdout: [[', res, ']]')
+  elif videotype == VType.V160_139:
+    comm = VType.basecomm160_139.format(ytid=ytid)
+    print(comm)
+    p = Popen(comm, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    res = str(stderr)
+    print('OS command response stderr: [[', res, ']]')
+    if FORMAT_NOT_AVAILABLE_MSG.lower() in res.lower():
+      print(FORMAT_NOT_AVAILABLE_MSG, ':: Trying format', VType.V160_140)
+      return download_ytid(ytid, VType.V160_140)
+    res = str(stdout)
+    print('OS command response stdout: [[', res, ']]')
+  elif videotype == VType.V160_140:
+    comm = VType.basecomm160_140.format(ytid=ytid)
+    print(comm)
+    p = Popen(comm, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    res = str(stderr)
+    print('OS command response stderr: [[', res, ']]')
+    if FORMAT_NOT_AVAILABLE_MSG.lower() in res.lower():
+      print(FORMAT_NOT_AVAILABLE_MSG, ':: formats_tried', formats_tried)
+      return download_ytid(ytid, VType.V160_140)
+    res = str(stdout)
+    print('OS command response stdout: [[', res, ']]')
+  else:
+    print('Videotype', videotype, 'not recognized')
   return
 
 
