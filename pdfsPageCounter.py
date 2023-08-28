a3231fe8d52f5dedc81528de013b4a68b744839b
Luiz Lewis <livrosetc@yahoo.com.br>
2023-08-27 22:18:35 -0300

    Created renameReplaceWordsToDigitNumbersUpTo99.py; updated improved pdfsPageCounter.py; both can be run from an API where any available directory can be given;

--- pdfsPageCounter.py
+++ pdfsPageCounter.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 """
 This script renames pdf-files inserting the total page number according to a standard/protocal way.
-
-Example:
-  Suppose a filename is named:
-    'Book of Greek Philosophy.pdf'
-  Suppose further this pdf_file has 100 pages. The renaming will change it to:
-    'Book of Greek Philosophy.100p.pdf'
+(It uses library PyPDF2.)
 
 Usage:
-  pdfsPageCounter.py [<abs_directory>]
+  pdfsPageCounter.py [-p="<abs_directory>"]  [-y|-Y]
 
-Optional Parameter:
-  abs_directory: the directory under which the renames will occur. If None, current directory will be used.
+Optional Parameters:
+  1) abs_directory: the directory under which the renames will occur. If None, current directory will be used.
+  2) -y|-Y means autorename (ie without the user's confirmation (yes/no) in CLI-prompt)
+
+A simple example:
+1) Issuing command:
+  $ pdfsPageCounter.py
+2) Now, suppose there is a filename (in the current running folder) named:
+  'Book of Greek Philosophy.pdf'
+3) Suppose further this pdf_file has 100 pages. The renaming will change it to:
+  'Book of Greek Philosophy.100p.pdf'
+4) Notice that a confirmation (yes/no) in the CLI-prompt, in this case (ie, without the -y option), will show up.
+
+Previously, this script ran only under the current folder. In August 2023,
+  it was updated to accept any directory in the available folder-tree and then
+  can also be used as an API to other calling programs.
 """
-import glob
 import os
-import PyPDF2
+import sys
 from PyPDF2 import PdfFileReader
+from PyPDF2.errors import PdfReadError
 
 
 def check_n_get_number_from_strnumber_plus_p(strnumber_plus_p):
@@ -35,91 +44,122 @@ def check_n_get_number_from_strnumber_plus_p(strnumber_plus_p):
 
 class PageTotalPdfRenamer:
 
-  def __init__(self, basefolder_absdir='.'):
-    if basefolder_absdir is None or\
-       len(basefolder_absdir) == 0 or\
-       basefolder_absdir == '.' or\
-       not os.path.isdir(basefolder_absdir):
-      basefolder_absdir = os.path.abspath('.')
-    self.basefolder_absdir = basefolder_absdir
+  def __init__(self, p_basefolder_absdir=None, p_autoren_no_cli_confirm=False):
     self.seq = 0
     self.total_pdfs_in_folder = 0
-    self.pdfs = []  # this attribute is to be deleted after use
-    self.pdf_n_pagetotal_tuplelist = []
-    self.rename_tuplelist = []
+    self.pdffilenames = []  # this attribute is to be deleted after use
+    self.filename_n_pagetotal_tuplelist = []
+    self.renamepairs = []
     self.total_renames = 0
-    self.bool_confirmed = False
+    self.autoren_no_cli_confirm = p_autoren_no_cli_confirm
+    self.basefolder_absdir = p_basefolder_absdir
+    self.treat_basefolder_absdir()
 
-  def collect_pdfs_in_folder(self):
-    pdfs = glob.glob('*.pdf')
-    # for keeping a record of amounts, the pdf file list itself will be kept up until the end of script
-    self.total_pdfs_in_folder = 0 + len(pdfs)
-    # filter out those with page-total
-    for pdf_filename in pdfs:
-      pp = pdf_filename.split('.')
+  def treat_basefolder_absdir(self):
+    try:
+      if os.path.isdir(self.basefolder_absdir):
+        # it's okay, return
+        return
+      else:
+        # folder does not exist and exception has not been raised, set it to local 'running' folder and return
+        self.basefolder_absdir = os.path.abspath('.')
+        return
+    except (TypeError, ValueError):
+      # exception has been raised, set it to local 'running' folder (return happens in following)
+      self.basefolder_absdir = os.path.abspath('.')
+
+  def finish_collect_pdfs_excluding_those_already_with_pagesufix(self, pdffiles):
+    """
+    There is a strong supposition in the code below, ie filename.nnp.pdf the nnp should not collide with a coincide case
+    But would there be a coincide in such case, for the semantics of nnp (eg 257p) is 257 pdf-pages
+    :param pdffiles:
+    :return:
+    """
+    self.pdffilenames = []
+    for i, pdffile in enumerate(pdffiles):
+      pdffilename = os.path.split(pdffile)[1]
+      pp = pdffilename.split('.')
       try:
-        from_strnumber_plus_p = pp[-2]
+        from_strnumber_plus_p = pp[-2]  # there is a strong supposition in here (@see docstring above)
       except IndexError:
         continue
       number = check_n_get_number_from_strnumber_plus_p(from_strnumber_plus_p)
       if not number:  # notice there should not be a pdf with 0 pages (as 0 is equivalent to False)
-        self.pdfs.append(pdf_filename)
+        self.pdffilenames.append(pdffilename)
 
-  def collect_totalpage_for_pdfs(self):
+  def select_pdffiles_in_folder(self):
+    """
+    The method picks up all pdf files in the target (basedir) folder
+    Just a historical note: before script was updated to consider full os-paths, it used:
+      pdfs = glob.glob('*.pdf')  # because previously the running context was always the current executing directory
+    :return:
+    """
+    filenames = os.listdir(self.basefolder_absdir)
+    filenames = list(filter(lambda fn: fn.endswith('.pdf'), filenames))
+    pdffiles = [os.path.join(self.basefolder_absdir, fn) for fn in filenames]
+    pdffiles = list(filter(lambda f: os.path.isfile(f), pdffiles))
+    sorted(pdffiles)
+    # filter out those that have already a page-total sufix and then don't need it
+    return self.finish_collect_pdfs_excluding_those_already_with_pagesufix(pdffiles)
+
+  def collect_totalpage_in_each_pdf_for_all_pdfs(self):
     # for keeping just a record of amounts, the list itself will be deleted
-    total_pdfs = len(self.pdfs)
-    for i, pdf_filename in enumerate(self.pdfs):
+    afterprocess_filenames = []
+    for i, pdffilename in enumerate(self.pdffilenames):
       seq = i + 1
-      print(seq, '=>', pdf_filename)
-      filepath = os.path.join(self.basefolder_absdir, pdf_filename)
-      with open(filepath, 'rb') as fd:
+      pdffile = os.path.join(self.basefolder_absdir, pdffilename)
+      print(seq, '=>', pdffilename)
+      with open(pdffile, 'rb') as fd:
         try:
           pdf_obj = PdfFileReader(fd)
           n_of_pages = pdf_obj.getNumPages()
-        except (PyPDF2.errors.PdfReadError, ValueError):
+        except (PdfReadError, ValueError):  # PyPDF2.errors.PdfReadError
           continue
-        pdf_n_pagetotal_tuple = (pdf_filename, n_of_pages)
-        print(seq, '/', total_pdfs, 'finding total pages as', n_of_pages, 'for', pdf_filename)
+        afterprocess_filenames.append(pdffilename)
+        filename_n_pagetotal_tuple = (pdffilename, n_of_pages)
+        print(seq, '/', self.total_pdfs_in_folder, 'finding total pages as', n_of_pages, 'for', pdffilename)
         print('-'*30)
-        self.pdf_n_pagetotal_tuplelist.append(pdf_n_pagetotal_tuple)
-    del self.pdfs
+        self.filename_n_pagetotal_tuplelist.append(filename_n_pagetotal_tuple)
+    self.pdffilenames = afterprocess_filenames
 
   def print_totalpage_for_pdfs(self):
-    total_pdfs = len(self.pdfs)
-    for i, pdf_n_pagetotal_tuple in enumerate(self.pdf_n_pagetotal_tuplelist):
+    total_pdfs_for_rename = len(self.filename_n_pagetotal_tuplelist)
+    for i, filename_n_pagetotal_tuple in enumerate(self.filename_n_pagetotal_tuplelist):
       seq = i + 1
-      pagetotal = pdf_n_pagetotal_tuple[1]
-      pdfname = pdf_n_pagetotal_tuple[1]
-      print(seq, '/', total_pdfs, 'page total', pagetotal, pdfname)
+      pdffilename = filename_n_pagetotal_tuple[0]
+      pagetotal = filename_n_pagetotal_tuple[1]
+      print(seq, '/', total_pdfs_for_rename, 'page total', pagetotal, pdffilename)
 
-  def mount_renametuples(self):
-    for _, pdf_n_pagetotal_tuple in enumerate(self.pdf_n_pagetotal_tuplelist):
-      src_filename = pdf_n_pagetotal_tuple[0]
-      pagetotal = pdf_n_pagetotal_tuple[1]
-      src_filepath = os.path.join(self.basefolder_absdir, src_filename)
-      extless_name, _ = os.path.splitext(src_filename)
+  def generate_renamepairs(self):
+    self.renamepairs = []
+    for pdffile_n_pagetotal_tuple in self.filename_n_pagetotal_tuplelist:
+      oldfilename = pdffile_n_pagetotal_tuple[0]
+      pagetotal = pdffile_n_pagetotal_tuple[1]
+      extless_name, _ = os.path.splitext(oldfilename)
       if len(extless_name) < 3:
         continue
       if extless_name.endswith('.p'):
         extless_name = extless_name[:-2]
-      trg_filename = extless_name + '.' + str(pagetotal) + 'p.pdf'
-      trg_filepath = os.path.join(self.basefolder_absdir, trg_filename)
-      rename_tuple = (src_filepath, trg_filepath)
-      self.rename_tuplelist.append(rename_tuple)
+      oldfile = os.path.join(self.basefolder_absdir, oldfilename)
+      newfilename = extless_name + '.' + str(pagetotal) + 'p.pdf'
+      newfile = os.path.join(self.basefolder_absdir, newfilename)
+      rename_tuple = (oldfile, newfile)
+      self.renamepairs.append(rename_tuple)
 
   def show_pairs(self):
-    total_renamepdfs = len(self.rename_tuplelist)
-    for i, rename_tuple in enumerate(self.rename_tuplelist):
+    total_renamepdfs = len(self.renamepairs)
+    for i, rename_tuple in enumerate(self.renamepairs):
       seq = i + 1
       print('-'*30)
-      src_filepath, trg_filepath = rename_tuple
+      oldfile, newfile = rename_tuple
+      oldfilename = os.path.split(oldfile)[1]
+      newfilename = os.path.split(newfile)[1]
       print(seq, '/', total_renamepdfs, 'To Confirm-Rename:')
-      print('\t [', src_filepath, ']')
-      print('\t [', trg_filepath, ']')
+      print('\t [', oldfilename, ']')
+      print('\t [', newfilename, ']')
 
   def confirm_renames(self):
-    self.bool_confirmed = False
-    total_renamepdfs = len(self.rename_tuplelist)
+    total_renamepdfs = len(self.renamepairs)
     if total_renamepdfs == 0:
       print('-'*30)
       print('There are %d pdf files in running folder.' % self.total_pdfs_in_folder)
@@ -127,36 +167,51 @@ class PageTotalPdfRenamer:
       print('-'*30)
       return
     self.show_pairs()
-    screen_msg = 'Confirm the %d renames above ? (*Y/n) ' % len(self.rename_tuplelist)
+    if self.autoren_no_cli_confirm:
+      return True
+    screen_msg = 'Confirm the %d renames above ? (*Y/n) ' % len(self.renamepairs)
     ans = input(screen_msg)
     if ans in ['Y', 'y', '']:
-      self.bool_confirmed = True
+      return True
+    return False
 
   def rename_pairs(self):
-    total_renamepdfs = len(self.rename_tuplelist)
-    for i, rename_tuple in enumerate(self.rename_tuplelist):
+    for i, rename_tuple in enumerate(self.renamepairs):
       seq = i + 1
-      src_filepath, trg_filepath = rename_tuple
-      if not os.path.isfile(src_filepath):
+      oldfile, newfile = rename_tuple
+      if not os.path.isfile(oldfile):
         continue
-      if os.path.isfile(trg_filepath):
+      if os.path.isfile(newfile):
         continue
-      print(seq, '/', total_renamepdfs, 'Renaming:')
-      print(seq, src_filepath)
-      print(seq, trg_filepath)
-      os.rename(src_filepath, trg_filepath)
+      print(seq, '/', len(self.renamepairs), 'Renaming:')
+      oldfilename = os.path.split(oldfile)[1]
+      newfilename = os.path.split(newfile)[1]
+      print(seq, oldfilename)
+      print(seq, newfilename)
+      os.rename(oldfile, newfile)
       self.total_renames += 1
     print('Total renamed =', self.total_renames)
 
   def process(self):
-    self.collect_pdfs_in_folder()
-    self.collect_totalpage_for_pdfs()
-    self.mount_renametuples()
-    self.confirm_renames()
-    if self.bool_confirmed:
+    self.select_pdffiles_in_folder()
+    self.collect_totalpage_in_each_pdf_for_all_pdfs()
+    self.generate_renamepairs()
+    if self.confirm_renames():
       self.rename_pairs()
 
 
+def get_args():
+  p_autoren_no_cli_confirm = False
+  p_basefolder_absdir = None
+  for arg in sys.argv:
+    if arg.startswith('-p='):
+      p_basefolder_absdir = arg[len('-p='):]
+    elif arg in ['-y', '-Y']:
+      p_autoren_no_cli_confirm = True
+  return p_basefolder_absdir, p_autoren_no_cli_confirm
+
+
 if __name__ == '__main__':
-  renamer = PageTotalPdfRenamer()
+  basefolder_absdir, autoren_no_cli_confirm = get_args()
+  renamer = PageTotalPdfRenamer(basefolder_absdir, autoren_no_cli_confirm)
   renamer.process()
---
+++ renameReplaceWordsToDigitNumbersUpTo99.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+"""
+renameReplaceWordsToDigitNumbersUpTo99.py
+This script transforms a phrase written number, limited to 99, to its digit-form.
+
+Examples:
+  'Twenty Seven Metallic Glass.26p.pdf' becomes '27 Metallic Glass.26p.pdf'
+  'Three Properties of Silicon Crystals.21p.pdf' becomes '3 Properties of Silicon Crystals.21p.pdf'
+
+At this version, it works for separated words such "Twenty Seven", not with "Twenty-Seven" separated with a dash (-)
+  TO-DO: (for a next version) allow dashed separated phrase numbers such as Twenty-Seven or thirty-nine
+
+Usage:
+$ renameReplaceWordsToDigitNumbersUpTo99.py [-p=<basefolder_absdir>] [-e=<extension>] [-y|Y]
+
+Where the all three optional parameters are:
+  1) -p=<basefolder_absdir> is the directory where the renaming will take place (default is the local folder)
+  2) [-e=<extension>] is the file extension where renaming will happen (e.g. "pdf", also the default one)
+  3) -y or -Y means "yes" autoren_no_cli_confirm ie no CLI confirmation will be asked for renaming (default is "no")
+
+Example:
+$ renameReplaceWordsToDigitNumbersUpTo99.py -p="/home/datafiles/book1" -e=txt -y
+"""
+import os
+import sys
+first_nineteen = [
+  'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+]
+dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninty']
+
+
+def get_number_from_phrase_upto99(phrase):
+  """
+  This function works limited to a phrase written number up to 99.
+  Also composed number must be separated by a space (gap), example: twenty seven (instead of twenty-seven)
+  :param phrase:
+  :return:
+  """
+  try:
+    pp = phrase.split(' ')
+  except ValueError:
+    return None, phrase
+  if len(pp) < 2:
+    return None, phrase
+  left_word = pp[0].lower()
+  if left_word in first_nineteen:
+    unit_number = first_nineteen.index(left_word)  # returns unit number as a digit (0, 1, 2, 3..., 9)
+    newphase = str(unit_number) + ' ' + ' '.join(pp[1:])
+    return unit_number, newphase
+  # dozen_number = 0
+  if left_word in dozens:
+    dozen_number = dozens.index(left_word) * 10
+  else:
+    return None, phrase
+  right_word = pp[1].lower()
+  unit_number = 0
+  if right_word in first_nineteen:
+    unit_number = first_nineteen.index(right_word)
+  twodigitnumber = dozen_number + unit_number
+  if unit_number == 0:
+    remainder = ' '.join(pp[1:])  # notice twenty (thirty etc) is not written twenty zero ie the second word is not used
+  else:
+    remainder = ' '.join(pp[2:])
+  newphrase = str(twodigitnumber) + ' ' + remainder
+  return twodigitnumber, newphrase
+
+
+class WordsToDigitsReplaceRenamer:
+  DEFAULT_DOTEXTENSION = '.pdf'
+
+  def __init__(self, p_basefolder_absdir=None, p_extension=None, p_autoren_no_cli_confirm=False):
+    self.seq = 0
+    self.total_pdfs_in_folder = 0
+    self.filenames = []  # this attribute is to be deleted after use
+    self.renamepairs = []
+    self.total_renames = 0
+    self.dotextension = p_extension
+    self.autoren_no_cli_confirm = p_autoren_no_cli_confirm
+    self.basefolder_absdir = p_basefolder_absdir
+    self.treat_dotextension()
+    self.treat_basefolder_absdir()
+
+  def treat_dotextension(self):
+    if self.dotextension is None:
+      self.dotextension = self.DEFAULT_DOTEXTENSION
+      return
+    self.dotextension = str(self.dotextension)
+    self.dotextension.lstrip('.')  # in case there are more than one dot (eg '....pdf')
+    self.dotextension = '. ' + self.dotextension  # restablish the starting dot '.'
+
+  def treat_basefolder_absdir(self):
+    try:
+      if os.path.isdir(self.basefolder_absdir):
+        # it's okay, return
+        return
+      else:
+        # folder does not exist and exception has not been raised, set it to local 'running' folder and return
+        self.basefolder_absdir = os.path.abspath('.')
+        return
+    except (TypeError, ValueError):
+      # exception has been raised, set it to local 'running' folder (return happens in following)
+      self.basefolder_absdir = os.path.abspath('.')
+
+  def select_files_in_folder(self):
+    filenames = os.listdir(self.basefolder_absdir)
+    filenames = filter(lambda f: f.endswith(self.dotextension), filenames)
+    files = [os.path.join(self.basefolder_absdir, fn) for fn in filenames]
+    files = filter(lambda f: os.path.isfile(f), files)
+    sorted(files)
+    self.filenames = [os.path.split(f)[1] for f in files]
+
+  def process_n_form_renamepairs(self):
+    for filename in self.filenames:
+      _, newfilename = get_number_from_phrase_upto99(filename)
+      if newfilename == filename:
+        continue
+      renamepair = (filename, newfilename)
+      self.renamepairs.append(renamepair)
+    # self.filenames is no longer necessary from here, for translated files, if any, were stored in self.renamepairs
+    del self.filenames
+
+  def show_pairs(self):
+    """
+
+    :return:
+    """
+    for i, renamepair in enumerate(self.renamepairs):
+      seq = i + 1
+      print('-'*30)
+      old_filename, new_filename = renamepair
+      print(seq, '/', len(self.renamepairs), 'To Confirm-Rename:')
+      print('\t [', old_filename, ']')
+      print('\t [', new_filename, ']')
+    print('In folder:', self.basefolder_absdir)
+
+  def confirm_renames(self):
+    if len(self.renamepairs) == 0:
+      print('No files found to rename.')
+      print('-'*30)
+      return
+    print('There are %d renamable files in the target folder.' % len(self.renamepairs))
+    print('-'*30)
+    self.show_pairs()
+    if self.autoren_no_cli_confirm:
+      return True
+    screen_msg = 'Confirm the %d renames above ? (*Y/n) [y, Y or empty-ENTER means yes] ' % len(self.renamepairs)
+    ans = input(screen_msg)
+    if ans in ['Y', 'y', '']:
+      return True
+    return False
+
+  def rename_pairs(self):
+    for i, renamepair in enumerate(self.renamepairs):
+      seq = i + 1
+      old_filename, new_filename = renamepair
+      oldfile = os.path.join(self.basefolder_absdir, old_filename)
+      newfile = os.path.join(self.basefolder_absdir, new_filename)
+      if not os.path.isfile(oldfile):
+        print('missing file', oldfile, ':: looping to next.')
+        continue
+      if os.path.isfile(newfile):
+        print('same name file', newfile, ':: looping to next.')
+        continue
+      print(seq, '/', len(self.renamepairs), 'Renaming:')
+      print(seq, old_filename)
+      print(seq, new_filename)
+      os.rename(oldfile, newfile)
+      self.total_renames += 1
+    print('Total renamed =', self.total_renames)
+
+  def process(self):
+    self.select_files_in_folder()
+    self.process_n_form_renamepairs()
+    if self.confirm_renames():
+      self.rename_pairs()
+
+
+def adhoc_test():
+  t = 'Twenty Seven Metallic Glass.26p'
+  ans = get_number_from_phrase_upto99(t)
+  print('prase', t)
+  print('result', ans)
+  t = 'Three Properties of Silicon Crystals.21p'
+  ans = get_number_from_phrase_upto99(t)
+  print('prase', t)
+  print('result', ans)
+
+
+def get_args():
+  p_basefolder_absdir = None
+  p_extension = None
+  p_autoren_no_cli_confirm = False
+  for arg in sys.argv:
+    if arg.startswith('-p='):
+      p_basefolder_absdir = arg[len('-p='):]
+    elif arg.startswith('-e='):
+      p_extension = arg[len('-e='):]
+    elif arg in ['-y', '-Y']:
+      p_autoren_no_cli_confirm = True
+  return p_basefolder_absdir, p_extension, p_autoren_no_cli_confirm
+
+
+if __name__ == '__main__':
+  """
+  adhoc_test()
+  """
+  basefolder_absdir, extension, autoren_no_cli_confirm = get_args()
+  renamer = WordsToDigitsReplaceRenamer(basefolder_absdir, extension, autoren_no_cli_confirm)
+  renamer.process()
