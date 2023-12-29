27ecced1077211314667ffc82dd44f5d5262b2fd
Luiz Lewis <livrosetc@yahoo.com.br>
2023-12-29 20:54:58 -0300

    Upgraded, to Python3, unzipEachToFolder.py;

--- unzipEachToFolder.py
+++ unzipEachToFolder.py
@@ -1,17 +1,35 @@
-#!/usr/bin/env python2
-import glob, os
+#!/usr/bin/env python3
+"""
+unzipEachToFolder.py
+  unzips each zipfile in the current folder.
 
-zips = glob.glob('*.zip')
+Upgraded script from Python2 to Python3 on 2023-12-25
+  (with the oportunity of maching only having Python3...)
+"""
+import glob
+import os
 
-c=0
-for zip in zips:
-    folder = zip[:-4]
+
+def unzip_all_files_in_dir():
+  zips = glob.glob('*.zip')
+  n_of_unzipped = 0
+  for ezip in zips:
+    folder = ezip[:-4]
     if os.path.isdir(folder):
-        continue
-    comm = 'unzip "'+zip+'" -d "' + folder + '"'
-    print c+1, 'unzipping', zip
-    retValue = os.system(comm)
-    if retValue == 0:
-        c += 1
-print 'Total unzipped:', c, '||| Total no. of zip files:', len(zips)
+      continue
+    scrmsg = f"{n_of_unzipped+1} unzipping file [{ezip}]"
+    print(scrmsg)
+    comm = f'unzip "{ezip}" -d "{folder}"'
+    retval = os.system(comm)
+    if retval == 0:
+      n_of_unzipped += 1
+  scrmsg = f"Total unzipped: {n_of_unzipped} ||| Total n. of zip files: {len(zips)}"
+  print(scrmsg)
 
+
+def process():
+  unzip_all_files_in_dir()
+
+
+if __name__ == "__main__":
+  process()
