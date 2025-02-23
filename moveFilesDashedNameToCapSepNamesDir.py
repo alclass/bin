a5dba330ed49164498b779de095b616c097c7ccb
Luiz Lewis <livrosetc@yahoo.com.br>
2025-02-23 16:49:30 -0300

    Updating moveFilesDashedNameToCapSepNamesDir.py (the dir-discovering is still beta, the default-dir is already working);

--- moveFilesDashedNameToCapSepNamesDir.py
+++ moveFilesDashedNameToCapSepNamesDir.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-moveFilesNameDashConvention
+~/bin/moveFilesDashedNameToCapSepNamesDir
 
 This script moves files into directories that bind to a certain specified convention.
 
@@ -52,6 +52,7 @@ The script will move this file to:
 """
 import glob
 import os
+import sys
 import shutil
 from pathlib import Path
 default_dot_ext = '.mp4'
@@ -99,115 +100,232 @@ def get_childpath_based_on_secondleveldir_n_name(secondlevel_dirpath, cap_sep_na
   return None
 
 
-class FilenameItem:
-  def __init__(self, dashedname):
-    self.dashedname = None
-    self.treat_dashedname(dashedname)
-    self.capitalized_letters = []
-    self._letter_n_doubleletter_middledirs = None  # e.g. Albert-Einsten makes 'A/AL' the AL from [Al]bert
-    self.capitalized_spaced_name = None
-    self._namelist = None
-    self.spaced_names = ''
-    self.spread_object()
+def create_dirpaths(paths_not_existing):
+  for ppath in paths_not_existing:
+    if os.path.isdir(ppath):
+      continue
+    print('Creating dirpath', ppath)
+    os.makedirs(ppath)
 
-  def treat_dashedname(self, dashedname):
-    self.dashedname = 'Unknown-Unknown'
+class FilenameItem:
+
+  conventioned_suffix_for_topdirname = 'yyyy cc'
+  def __init__(self, orig_filename):
+    self.orig_filename = orig_filename
+    self._names = None
+    self.dashednames_str = None
+    self.set_dashnames_str()
+    # self.treat_dashedname(dashedname)
+
+  def set_dashnames_str(self):
+    """
+    The convention says the dash-names string should be the 2nd word in orig_filename
+    Taking this 2nd word, if it doesn't have a 'dash', name is single ie it's a one-word name
+    """
     try:
-      pp = dashedname.split(' ')
-      if len(pp) > 0:
-        self.dashedname = pp[0]
+      pp = self.orig_filename.split(' ')
+      if len(pp) > 1:
+        self.dashednames_str = pp[1]
     except ValueError:
-      pass
+      errmsg = 'orig-filename [%s] does not follow the convention' % self.orig_filename
+      raise ValueError(errmsg)
+
+  @property
+  def names(self):
+    """
+    Each name in names should have, if it's not, its first letter capitalized
+    """
+    if self._names is not None:
+      return self._names
+    self._names = []
+    for name in self.dashednames_str.split('-'):
+      if name == '':
+        continue
+      if name[0] == name[0].lower():
+        if len(name) == 1:
+          name = name.upper()
+        else:
+          name = name[0].upper() + name[1:]
+      self._names.append(name)
+    return self._names
+
+  @property
+  def spaced_names_str(self):
+    return ' '.join(self.names)
+
+  @property
+  def capitalized_letters_from_eachname_aslist(self):
+    capletters = []
+    for currname in self.names:
+      capletters.append(currname[0])
+    return capletters
+
+  @property
+  def firstcapletter(self):
+    if len(self.names) > 0:
+      firstname = self.names[0]
+      if len(firstname) > 0:
+        return firstname[0:1]
+    return None
+
+  @property
+  def firsttwocapletters(self):
+    if len(self.names) > 0:
+      firstname = self.names[0]
+      if len(firstname) > 1:
+        return firstname[0:2]
+    return None
 
   @property
   def letter_n_doubleletter_middledirs(self):
-    if self._letter_n_doubleletter_middledirs is not None:
-      return self.letter_n_doubleletter_middledirs
-    firstletter = self.capitalized_spaced_name[0]
-    doubleletter = self.capitalized_spaced_name[0:2]
-    middledirs = firstletter + '/' + doubleletter
-    self._letter_n_doubleletter_middledirs = middledirs
-    return intermediate_dis
+    """
+    e.g. Albert-Einsten makes 'A/AL' the AL from [Al]bert
+    """
+    first = self.firstcapletter
+    if first is None:
+      return None
+    second = self.firsttwocapletters
+    # notice that the second lever may not be capitalized, so do it here
+    if second is None:
+      second = first
+    second = second.upper()
+    middledirs_str = first + '/' + second
+    return middledirs_str
 
   @property
-  def namelist(self):
-    if self._namelist is not None:
-      return self._namelist
-    self._namelist = []
-    # self.dashedname was treated upon __init__() so the for below is safe
-    for name in self.dashedname.split('-'):
-      self._namelist.append(name)
-    return self._namelist
+  def capitalized_firsttwoletters_from_firstname_aslist(self):
+    middledirs_convention_str = self.letter_n_doubleletter_middledirs
+    if middledirs_convention_str:
+      return list(middledirs_convention_str.split('/'))
+    return None
 
-  def span_capitalized_letters(self):
+  def form_conventioned_topdirname(self):
+    return self.spaced_names_str + ' ' + self.conventioned_suffix_for_topdirname
+
+  def form_basedir_plus_lettermiddlepath(self, basedir):
     """
-      firstletter = self.src_filenames[0].upper()
-      first_2letters = self.src_filenames[0:1].upper()
-      middle_dirs = first_2letters + '/' + first_2letters
-      spaced_name = derive_firstletter_capitalized_names_from_dashed_lowercase(eachfile)
-    :return:
+    To be called from an AlphabetMover object, or any one that 'knows' basedir
+    The result path may or not exist, the caller should find out (maybe ask its creation...)
     """
-    self.capitalized_letters = []
-    for name in self.namelist:
-      if len(name) > 0:
-        self.capitalized_letters = name[0].upper()
+    return os.path.join(basedir, self.letter_n_doubleletter_middledirs)
 
-  def derive_firstletter_capitalized_names_from_dashed_lowercase(self):
-    self.capitalized_spaced_name = derive_firstletter_capitalized_names_from_dashed_lowercase(self.dashedname)
+  def form_basedir_lettermiddlepath_n_convention(self, basedir):
+    middlepath = self.form_basedir_plus_lettermiddlepath(basedir)
+    conventioned_topdirname = self.form_conventioned_topdirname()
+    return os.path.join(middlepath, conventioned_topdirname)
 
-  def spread_object(self):
-    self.derive_firstletter_capitalized_names_from_dashed_lowercase()
-    self.span_capitalized_letters()
+  def discover_an_folder_with_a_changedsufix(self, basedir):
+    middlepath = self.form_basedir_plus_lettermiddlepath(basedir)
+    entries = os.listdir(middlepath)
+    direntries = map(lambda e: os.path.join(middlepath, e), entries)
+    dirpaths = filter(os.path.isdir, direntries)
+    for dirpath in dirpaths:
+      _, foldername = os.path.split(dirpath)
+      if foldername.startswith(self.spaced_names_str):
+        # found !
+        return dirpath
+    return None
 
-  def derive_letter_n_doubleletter_second_level_dirpath(self, basedir):
-    """
-    To be called by an AlphabetMover object which 'knows' basedir
-    :param basedir:
-    :return:
-    """
-    return os.path.join(basedir, self.intermediate_letter_n_doubleletter)
+  def get_basedir_lettermiddlepath_name_or_convention(self, basedir):
+    dirpath = self.discover_an_folder_with_a_changedsufix(basedir)
+    if dirpath is None:
+      return self.form_basedir_lettermiddlepath_n_convention(basedir)
+    return dirpath
 
-  def as_dict(self):
-    _as_dict = {
-      'dashedname': {self.dashedname},
-      'capitalized_spaced_name': {self.capitalized_spaced_name},
-      'capitalized_letters': {self.capitalized_letters},
-      'namelist': {self.namelist},
-      'spaced_names': {self.spaced_names},
+  def as_str_dict(self):
+    _as_str_dict = {
+      'dashednames_str': {self.dashednames_str},
+      'spaced_names_str': {self.spaced_names_str},
+      'capitalized_letters_from_eachname_aslist': {str(self.capitalized_letters_from_eachname_aslist)},
+      'capitalized_firsttwoletters_from_firstname_aslist': \
+        {str(self.capitalized_firsttwoletters_from_firstname_aslist)},
+      'names': {str(self.names)},
     }
-    return _as_dict
+    return _as_str_dict
 
   def __str__(self):
     outstr = """
-      dashedname = [{dashedname}] | capitalized_spaced_name = [{capitalized_spaced_name}]
-      capitalized_letters = {capitalized_letters} | namelist = {namelist}
-      spaced_names = {spaced_names}
-    """.format(**self.as_dict())
+      dashednames_str = [{dashednames_str}] | spaced_names_str = [{spaced_names_str}]
+      capitalized_letters_from_eachname_aslist = {capitalized_letters_from_eachname_aslist}
+      capitalized_firsttwoletters_from_firstname_aslist = {capitalized_firsttwoletters_from_firstname_aslist}
+      names = {names}
+    """.format(**self.as_str_dict())
     return outstr
 
 
 class AlphabetFileMover:
 
-  def __init__(self, given_base_dir=None, dot_ext=None):
+  def __init__(self, basedir, dot_ext=None, sourcedir=None):
     self.dot_ext = dot_ext
     self.treat_dot_ext()
+    self.sourcedir = sourcedir
+    self.treat_sourcedir()
     self.src_filenames = []
     self.move_pair_list = []
-    self.basedir = None
-    self.given_base_dir = given_base_dir
-    self.executing_dir = None
-    self.determine_basedir()
-    self.process()
+    self.paths_not_existing = []
+    self.basedir = basedir
+    self.treat_basedir()
     # 3) find among target folder paths those that do not exist
 
   def verify_target_folders_existence(self):
-    pass
+    """
+    This function looks up all target dirpaths checking its existence
+    It stores all non-existing to an attribute that will be used
+      next on in the object's processing order
+    """
+    self.paths_not_existing = []
+    for movepair in self.move_pair_list:
+      targetpath = movepair[1]
+      if not os.path.isdir(targetpath):
+        self.paths_not_existing.append(targetpath)
 
   def create_target_folders_if_needed(self):
-    pass
+    """
+    # user confirmation
+    """
+    if len(self.paths_not_existing) == 0:
+      return
+    for i, targetpath in enumerate(self.paths_not_existing):
+      n_path = i + 1
+      scrmsg = '%d targetpath [%s] does not exist.' % (n_path, targetpath)
+      print(scrmsg)
+    scrmsg = 'Confirm creation of the %d paths above ? (Y/n) [ENTER] means Yes ' % \
+             len(self.paths_not_existing)
+    ans = input(scrmsg)
+    if ans in ['Y', 'y', '']:
+      create_dirpaths(self.paths_not_existing)
+
 
   def move_source_files_to_target_folders(self):
-    pass
+    if len(self.move_pair_list) == 0:
+      print('No files to move.')
+      return
+    for i, movepair in enumerate(self.move_pair_list):
+      seq = i + 1
+      filename = movepair[0]
+      sourcefilepath = os.path.join(self.sourcedir, filename)
+      targetpath = movepair[1]
+      if os.path.isfile(sourcefilepath) and os.path.isdir(targetpath):
+        print(seq, 'Moving')
+        print('\tFROM :', sourcefilepath)
+        print('\tTO   :', targetpath)
+    scrmsg = 'Confirm moving the %d files above ? (Y/n) [ENTER] means Yes ' % len(self.move_pair_list)
+    ans = input(scrmsg)
+    if ans not in ['Y', 'y', '']:
+      print('Not moving and returning.')
+      return
+    print('='*40)
+    print('Initiating moving')
+    print('='*40)
+    for i, movepair in enumerate(self.move_pair_list):
+      seq = i + 1
+      filename = movepair[0]
+      sourcefilepath = os.path.join(self.sourcedir, filename)
+      targetpath = movepair[1]
+      shutil.move(sourcefilepath, targetpath)
+      print(seq, 'Moved')
+      print('\tFROM :', sourcefilepath)
+      print('\tTO   :', targetpath)
 
   def treat_dot_ext(self):
     if self.dot_ext is None:
@@ -215,28 +333,21 @@ class AlphabetFileMover:
     if not self.dot_ext.startswith('.'):
       self.dot_ext = '.' + self.dot_ext
 
-  def determine_basedir(self):
-    if self.given_base_dir is None:
-      self.position_to_default_basefolder()
-    else:
-      self.position_to_given_basefolder()
-    print('basedir =', self.basedir)
+  def treat_sourcedir(self):
+    if self.sourcedir is None:
+      # default to current (running) directory
+      self.sourcedir = os.path.abspath(os.curdir)
+    if not os.path.isdir(self.sourcedir):
+      errmsg = 'Source directory [%s] does not exist.' % self.sourcedir
+      raise OSError(errmsg)
 
-  def position_to_default_basefolder(self):
-    self.executing_dir = os.path.abspath('.')
-    self.executing_dir = Path(self.executing_dir)
-    self.src_filenames = glob.glob('*.mp4')
-    sorted(filenames)
-    self.basedir = self.executing_dir.parent.parent
-
-  def gather_names_dir_pair_list(self):
-    self.move_pair_list = []
-    for src_filename in self.src_filenames:
-      pp = src_filename.split(' ')
-      firstsupposeddashedname = pp[1]
-      cap_sep_name = treat_name_to_move(firstsupposeddashedname)
-      move_pair = (filename, cap_sep_name)
-      self.move_pair_list.append(move_pair)
+  def treat_basedir(self):
+    if self.basedir is None:
+      # default to parent's parent directory
+      self.basedir = os.path.abspath('../..')
+    if not os.path.isdir(self.basedir):
+      errmsg = 'base directory [%s] does not exist.' % self.basedir
+      raise OSError(errmsg)
 
   def deduce_target_folderpaths_file_by_file(self):
     """
@@ -244,18 +355,19 @@ class AlphabetFileMover:
     """
     for eachfile in self.src_filenames:
       fileitem = FilenameItem(eachfile)
-      move_to_dir = fileitem.derive_letter_n_doubleletter_second_level_dirpath(self.basedir)
+      move_to_dir = fileitem.form_basedir_lettermiddlepath_n_convention(self.basedir)
       pair = (eachfile, move_to_dir)
       self.move_pair_list.append(pair)
+
 
   def gather_source_files_to_move_to_dirs(self):
     """
     Step 1: gather_source_files_to_move_to_dirs
     """
-    files_in_currfolder = os.listdir(self.executing_dir)
-    self.src_filenames = list(filter(lambda f: f.endswith(self.dot_ext), files_in_currfolder))
+    filenames = os.listdir(self.sourcedir)
+    self.src_filenames = list(filter(lambda f: f.endswith(self.dot_ext), filenames))
 
-  def process(self):
+  def process_move(self):
     # 1) gather local dir source files
     self.gather_source_files_to_move_to_dirs()
     # 2) deduce target folder paths
@@ -266,6 +378,38 @@ class AlphabetFileMover:
     self.create_target_folders_if_needed()
     # 5) move source files to target folders
     self.move_source_files_to_target_folders()
+
+  def as_str_dict(self):
+    _as_str_dict = {
+      'basedir': {self.basedir},
+      'dot_ext': {self.dot_ext},
+      'sourcedir':  {self.sourcedir},
+    }
+    return _as_str_dict
+
+  @property
+  def filenamelist_str(self):
+    _filenamelist_str = ''
+    for eachfile in self.src_filenames:
+      _filenamelist_str += eachfile + '\n'
+    return _filenamelist_str
+
+  @property
+  def movepairlist_str(self):
+    _movepairlist_str = ''
+    for eachpair in self.move_pair_list:
+      _movepairlist_str += str(eachpair) + '\n'
+    return _movepairlist_str
+
+  def __str__(self):
+    header = """
+    basedir = {basedir}
+    dot_ext = {dot_ext}
+    sourcedir = {sourcedir}
+    """.format(**self.as_str_dict())
+    outstr = header + '\n' + self.filenamelist_str
+    outstr = outstr + '\n' + self.movepairlist_str
+    return outstr
 
 
 def find_second_level_dirpath_from_name(self, cap_sep_name):
@@ -297,10 +441,37 @@ def move_pairs(move_pair_list):
       print('Moving...')
       shutil.move(filename, targetentry)
   
+def get_args():
+  basedir = None
+  dot_ext = None
+  sourcedir = None
+  for arg in sys.argv:
+    print(arg)
+    if arg.startswith('-b='):
+      basedir = arg[len('-b='):]
+    elif arg.startswith('-e='):
+      dot_ext = arg[len('-e='):]
+      if not dot_ext.startswith('.'):
+        dot_ext = '.' + dot_ext
+    elif arg.startswith('-s='):
+      sourcedir = arg[len('-s='):]
+  return basedir, dot_ext, sourcedir
+
+
+def adhoc_test1():
+  it = FilenameItem("'10' albert-einstein relativity.")
+  print(it)
+  dirpath = it.form_basedir_plus_lettermiddlepath('/dados/Science/Phy')
+  print(dirpath)
 
 def process():
-  move_pair_list = process_names()
-  move_pairs(move_pair_list)
+  """
+  """
+  basedir, dot_ext, sourcedir = get_args()
+  print('Params')
+  print('basedir | dot_ext | sourcedir', basedir, ' | ', dot_ext, ' | ', sourcedir)
+  alphabetmover = AlphabetFileMover(basedir, dot_ext, sourcedir)
+  alphabetmover.process_move()
 
 
 if __name__ == '__main__':
---
+++ todaysdate_as_yyyymmdd.sh
@@ -0,0 +1,3 @@
+# displays date in format yyyy-mm-dd
+# ref (among others) https://linuxsimply.com/bash-scripting-tutorial/basics/examples/date-format/
+date +%Y-%m-%d
