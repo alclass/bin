50bb9a33a3d36662c013599af436a8f1318150e6
Luiz Lewis <livrosetc@yahoo.com.br>
2025-07-27 17:06:29 -0300

    Improved docstr of uTubeInsertRemoveYtidUpDirSqlite.py;

--- uTubeInsertRemoveYtidUpDirSqlite.py
+++ uTubeInsertRemoveYtidUpDirSqlite.py
@@ -1,54 +1,58 @@
 #!/usr/bin/env python3
 """
-uTubeInsertRemoveYtidUpDirSqlite.py
+~/bin/uTubeInsertRemoveYtidUpDirSqlite.py
+  A dispatcher to the same named script in the DirTree codebase.
 
-(notice that dlYouTubeWithIdsOnTxtFile3.py does the same but using a different approach)
+(Notice that similar named dlYouTubeWithIdsOnTxtFile3.py does the same, but uses a different approach.)
 
-This scripts is a sort of caller-dispatcher
-  ie it links to an application installed elsewhere (the elsewhere-app)
-  and calls its process() function passing sys.argv from here
+This script is a sort of caller-dispatcher:
+  a) i.e., it links to an application installed elsewhere (the elsewhere-app)
+  b) and calls its process() (via if __name__) function passing sys.argv from here
+  c) in the future (a TODO), this app may be included within PYTHONPATH
+     from where the app may be called directly, obsoleting this dispatcher
 
-* In an organization note, the PyCharm IDE complains about the "remote" variables,
-  for the IDE does not found them "abroad" (it seems not to be looking into the __all__ list_.
+* As an organizational note, the PyCharm IDE complains about the "remote" variables,
+  for the IDE does not found them "abroad" (it seems not to be looking into the __all__ list_).
 
 The original (elsewhere-app) script does the following:
-1) reads a list of ytids (from default filename 'youtube-ids.txt' (its name is configured)
-2) looks up for each ytid its inclusion in a sqlite table (in a db configured)
-3) those ytids not stored in the sqlite-db will become the set for downloading
+1) reads a list of ytids (from default filename 'youtube-ids.txt' (its name is configured);
+2) for each ytid, if not in db, insert it into a sqlite table (in a db-configured sqlitefile);
+3) those ytids not stored in the sqlite-db will form the to-download set;
 
-In a nut shell:
-  The main purpose is not to download a formally downloaded video.
-  The purpose is to avoid redownloading an already-downloaded youtube video.
+In a nut-shell:
+  The main purpose is to avoid to download a previously (and kept) downloaded video.
 
 Notice also that the elsewhere-app does the storing of downloaded videos with its ytids.
 
 -------------------
 The code below was not essential to this script
-  (because it dynamically imports the elsewhere app)
+  (because it dynamically imports the elsewhere-app)
 except ImportError:
   # ModuleNotFoundError
   print('Error: module', __all__, 'not found: see directory path for finding the app.')
   sys.exit(1)
 
 
-Notice on the config variable PYMIRROAPP_PATH
+Notice about the config variable PYMIRROAPP_PATH:
 
-The following config variable [PYMIRROAPP_PATH] has been 'conventioned'
-  here though its appname itself has been changed from time to time.
+  The following config variable [PYMIRROAPP_PATH] has been 'conventioned' here,
+    though its appname itself has been changed from time to time.
 
 So attention must be taken on the corresponding path set in bin_local_settings.py
 The value set there is not in git repo, ie app may be installed anywhere in the os-dirtree
 and the user must place its basefolderpath there.
+
+  Another solution is to install the app to a PYTHONPATH path (a TODO).
 """
 import sys
 from bin_local_settings import PYMIRROAPP_PATH
 appsrootpath = PYMIRROAPP_PATH
 sys.path.insert(1, appsrootpath)
-from commands import uTubeInsertRemoveYtidUpDirSqlite as ins_missing_in_db
+from commands import uTubeInsertRemoveYtidUpDirSqlite as insertMissingInDb
 
 
 def dispatch():
-    return ins_missing_in_db.process(sys.argv)
+    return insertMissingInDb.process(sys.argv)
 
 
 def adhoctest1():
