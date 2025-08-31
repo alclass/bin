4dea436538db897efc4cf2806f51137d58c67466
Luiz Lewis <livrosetc@yahoo.com.br>
2025-08-31 14:52:11 -0300

    Created uTubeShowYtidRepeatsInDirTree.py; improved renameNumberPrefixToAnotherPartByRegex.py;

--- renameNumberPrefixToAnotherPartByRegex.py
+++ renameNumberPrefixToAnotherPartByRegex.py
@@ -7,14 +7,16 @@ Renames the number prefix in files in a folder
 At the time of writing, the captured regex is a number within parentheses and its last 3 digits are discarded.
   TODO improve this captured regex so that it can be input from the CLI parameters
 """
+import argparse
 import glob
 import os
 import re
+import sys
 # the regex contains an arbitrary first piece ".+?", then a gap "[ ]", then any numbers within parentheses "\((\d+?)\)"
 # which also makes up a group (group(1)), then a hyphen "\-", then an arbitrary last piece ".+?"
 # example: "bla foo bar (9812453452524352)- bla foo bar"
 # group(1) will match "9812453452524352"
-default_dot_ext = '.mp4'
+DEFAULT_FILE_EXTENSION = 'mp4'
 middle_regex_str = r".+?[ ]\((\d+?)\)\-.+?"
 middle_regex_cmp = re.compile(middle_regex_str)
 beginning_regex_str = r"^(\d+)[ ].+?"
@@ -23,13 +25,21 @@ number_of_rightsided_fixed_digits_to_stripout = 3
 # only the 3 first digits are sought for (this run has a following 200, it should become an input parameter)
 # actually, this last constant was discontinued for the rightside digit count above
 rightstrip_from_middle_number = '200'
+parser = argparse.ArgumentParser(description="Rename a prefix with info in another part.")
+parser.add_argument("--docstr", action="store_true",
+                    help="show docstr help and exit")
+parser.add_argument("--rundir", type=str, default=".",
+                    help="Directory in which this script willl execute, default to current dir.")
+parser.add_argument("--ext", type=str, default=DEFAULT_FILE_EXTENSION,
+                    help="Directory in which this script willl execute, default to current dir.")
+args = parser.parse_args()
 
 
 class Renamer:
 
   def __init__(self, basedir_abspath=None, dot_ext=None):
     self.basedir_abspath = basedir_abspath
-    self.dot_ext = dot_ext or default_dot_ext
+    self.dot_ext = dot_ext or DEFAULT_FILE_EXTENSION  # it will get a prepended dot if missing
     self.filenames = []
     self.rename_pairs = []
     self.renaming_confirmed = False
@@ -40,7 +50,7 @@ class Renamer:
     if self.basedir_abspath is None or not os.path.isdir(self.basedir_abspath):
       self.basedir_abspath = os.path.abspath('.')
     if not self.dot_ext.startswith('.'):
-      self.dot_ext = '+' + self.dot_ext
+      self.dot_ext = '.' + self.dot_ext
 
   def find_dot_ext_files_in_folder(self):
     files = glob.glob(self.basedir_abspath + '/*' + self.dot_ext)
@@ -54,13 +64,24 @@ class Renamer:
       if not match:
         continue
       # actually, it's not needed to form the new-name, but might be used to check filename starts with a 3-digit number
-      # first_number = match.group(1)
+      first_number = match.group(1)
       match = middle_regex_cmp.match(filename)
       if not match:
         continue
       middle_number = match.group(1)
       # middle_number = middle_number.rstrip(rightstrip_from_middle_number)
       middle_number = middle_number[: -number_of_rightsided_fixed_digits_to_stripout]
+      # check if they are equal, if so, don't go on, loop-continue
+      try:
+        first = int(first_number)
+        middle = int(middle_number)
+        if first == middle:
+          #  they are equal, nothing to do
+          continue
+        # reconstitute middle_number with zfill equals to the rightsided_fixed_digits
+        middle_number = str(middle).zfill(number_of_rightsided_fixed_digits_to_stripout)
+      except ValueError:
+        continue
       newname = middle_number + ' ' + filename[4:]
       if filename == newname:
         continue
@@ -105,6 +126,71 @@ class Renamer:
     self.do_rename()
 
 
+def show_docstrhelp_n_exit():
+  print(__doc__)
+  sys.exit(0)
+
+
+def get_cli_args():
+  """
+  Required parameters:
+    src_rootdir_abspath & trg_rootdir_abspath
+
+  Optional parameter:
+    resolution_tuple
+
+  :return: srctree_abspath, trg_rootdir_abspath, resolution_tuple
+  """
+  if args.docstr:
+    show_docstrhelp_n_exit()
+  rundir = args.rundir  # if not given, it defaults to "."
+  dot_ext = args.ext  # if not given, it defaults to "mp4" or DEFAULT_FILE_EXTENSION
+  if not dot_ext.startswith('.'):
+    dot_ext = '.' + dot_ext
+  return rundir, dot_ext
+
+
+def confirm_cli_args_with_user(ytids, dirpath, videoonlycode, audioonlycodes, nvdseq):
+  if not os.path.isdir(dirpath):
+    scrmsg = f"Source directory [{dirpath}] does not exist. Please, retry."
+    print(scrmsg)
+    return False
+  try:
+    int(videoonlycode)
+  except ValueError:
+    scrmsg = f"videoonlycode [{videoonlycode}] should be a number. Please, retry"
+    print(scrmsg)
+    return False
+  try:
+    audioonlycodes = audioonlycodes.split(',')
+  except ValueError:
+    scrmsg = (f"audioonlycodes [{audioonlycodes}] should be a number list with possible sufixes."
+              f" (@see also docstr for more info). Please, retry.")
+    print(scrmsg)
+    return False
+  charrule = '=' * 20
+  print(charrule)
+  print('Input parameters entered')
+  print(charrule)
+  scrmsg = f"""
+  => ytids = {ytids} | total = {len(ytids)} | sequential sufix for the 'vd' namemarker = {nvdseq} 
+  -------------------
+  => dirpath = [{dirpath}]
+  (confer default subdirectory "{default_videodld_tmpdir}" or other)
+  -------------------
+  => videoonlycode = {videoonlycode} | audioonlycodes = {audioonlycodes} 
+  """
+  print(scrmsg)
+  print(charrule)
+  scrmsg = "Are the parameters above okay? (Y/n) [ENTER] means Yes "
+  ans = input(scrmsg)
+  print(charrule)
+  confirmed = False
+  if ans in ['Y', 'y', '']:
+    confirmed = True
+  return confirmed, audioonlycodes
+
+
 def adhoctest1():
   s = "123 3' Competition among different schemata (179200)-7Ns8ktpL6FQ"
   print(s)
@@ -122,7 +208,11 @@ def adhoctest1():
 
 
 def process():
-  renamer = Renamer()
+  rundir, dot_ext = get_cli_args()
+  renamer = Renamer(
+    basedir_abspath=rundir,
+    dot_ext=dot_ext
+  )
   renamer.process()
 
 
---
+++ uTubeShowYtidRepeatsInDirTree.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+"""
+~/bin/uTubeShowYtidRepeatsInDirTree.py
+  Lists repeat ytids in a directory and also shows a ytid counting statistics.
+
+The DirTree functionality (which would show the counting for the whole directory tree)
+  has not yet been done. For the time being, it counts only the current or appointed directory.
+
+The first application (a kind of extended one) of this script was to help find
+  the differences of mp3's derived from mp4's in two directories.
+  Because the number of files was big, it helped find the missing ones, running this script twice,
+  once for each directory (mp3's & mp4's).
+"""
+import argparse
+import os.path
+import sys
+import localuserpylib.ytfunctions.yt_str_fs_vids_sufix_lang_map_etc as ytfs  # ytfs.is_str_a_ytid()
+parser = argparse.ArgumentParser(description="Show ytid stats in a folder or dirtree.")
+parser.add_argument("--docstr", action="store_true",
+                    help="show docstr help and exit")
+parser.add_argument("--dirpath", type=str,
+                    help="Directory from which ytid stats will be gather")
+args = parser.parse_args()
+
+
+def show_docstrhelp_n_exit():
+  print(__doc__)
+  sys.exit(0)
+
+
+def get_the_3_ytid_filename_pieces_or_nones(filename):
+  """
+  Returns the 3 conventioned parts (or pieces) of a ytid filename or, in case it fails the convention, a triple None.
+
+  The 3 ytid-conventioned filename parts are:
+    1 => the filename's prename
+    2 => its ytid
+    3 => its dot_ext (though optional, only a single extension is allowed)
+
+  The convention is:
+    => any previous name, a dash (hyphen), the 11-char ENC64 string and a dot extension
+
+  Examples:
+
+    1) file1-ABCabc123-_.mp4
+      =>
+        1-1 its prename is "file1"
+        1-2 its ytid is "ABCabc123-_"
+        1-3 its dot_ext ".mp4"
+
+    2) "bla foo bar-abc-123_DEF.mp3" => its ytid is "abc-123_DEF"
+      =>
+        1-1 its prename is "bla foo bar"
+        1-2 its ytid is "abc-123_DEF"
+        1-3 its dot_ext ".mp3"
+
+  """
+  name, dot_ext = os.path.splitext(filename)
+  try:
+    prename = name[:-12]
+    supposed_ytid = name[-12:]
+    if not supposed_ytid.startswith('-'):
+      return None, None, None
+    supposed_ytid = supposed_ytid[1:]
+    if ytfs.is_str_a_ytid(supposed_ytid):
+      ytid = supposed_ytid
+      return prename, ytid, dot_ext
+  except IndexError:
+    pass
+  return None, None, None
+
+
+def is_filename_a_ytid_conventioned_one(filename):
+  _, ytid, _ = get_the_3_ytid_filename_pieces_or_nones(filename)
+  if ytid is None:
+    return False
+  return True
+
+
+class YtidsInDirLookerUp:
+
+  def __init__(self, dir_abspath=None):
+    self.dir_abspath = dir_abspath
+    self.filenames = []
+    self.n_files = 0
+    self.ytid_n_filename_dict = {}
+    self.ytid_repeat_set = set()
+    self.ytidrepeat_filenames = []
+    self.treat_attrs()
+
+  def treat_attrs(self):
+    if self.dir_abspath is None or not os.path.isdir(self.dir_abspath):
+      self.dir_abspath = os.path.abspath('.')
+
+  @property
+  def n_ytid_files(self):
+    return len(self.ytid_n_filename_dict)
+
+  @property
+  def ytid_repeats(self):
+    return list(self.ytid_repeat_set)
+
+  @property
+  def n_ytid_repeats(self):
+    return len(self.ytidrepeat_filenames)
+
+  @property
+  def total_ytid_files(self):
+    n1 = self.n_ytid_files
+    n2 = self.n_ytid_repeats
+    return n1 + n2
+
+  def get_n_store_ytidfilenamesdict_in_dir(self):
+    entries = os.listdir(self.dir_abspath)
+    for entry in entries:
+      # it's assumed a ytid-conventioned dirname is not there
+      _, ytid, _ = get_the_3_ytid_filename_pieces_or_nones(entry)
+      if ytid is None:
+        continue
+      if ytid in self.ytid_n_filename_dict:
+        self.ytid_repeat_set.add(ytid)
+        self.ytidrepeat_filenames.append(entry)
+        continue
+      self.ytid_n_filename_dict[ytid] = entry
+
+  def process(self):
+    print(f'Processing dir {self.dir_abspath}')
+    self.get_n_store_ytidfilenamesdict_in_dir()
+    print(self)
+
+  def list_all_ytids(self):
+    for ytid in self.ytid_n_filename_dict:
+      print(ytid)
+
+  def __str__(self):
+    outstr = f"""
+    n_ytid_files = {self.n_ytid_files}
+    total_ytid_files = {self.total_ytid_files}
+    ytid_repeats = {self.ytid_repeat_set}
+    ytid_filename_repeats = {self.ytidrepeat_filenames}
+    n of excess repeats = {len(self.ytidrepeat_filenames)}
+    """
+    return outstr
+
+
+def get_args():
+  dirpath = args.dirpath or "."
+  return dirpath
+
+
+def adhoctest2():
+  test = """
+  filename without extension-gD0VfFTu6V8
+  """
+  lines = test.split('\n')
+  for i, line in enumerate(lines):
+    line = line.lstrip(' \t').rstrip(' \t\r\n')
+    triple = get_the_3_ytid_filename_pieces_or_nones(line)
+    prename, ytid, dot_ext = triple
+    seq = i + 1
+    scrmsg = f"{seq} | prename=[{prename}] ytid={ytid} dot_ext={dot_ext} [{line}]"
+    print(scrmsg)
+
+
+def adhoctest1():
+  """
+  Notice that the first test below shows that any extension is allowed for the convention,
+    but the one that has a double extension (dot_ext + dot_ext) "fails" the convention
+  """
+  test = """
+  248 3' How I see artificial intelligence now (248248)-gD0VfFTu6V8.cnv.mp3
+  248 3' How I see artificial intelligence now (248248)-gD0VfFTu6V8.mp3
+  248 3' How I see artificial intelligence now (248248)-gD0VfFTu6V8.empty
+  """
+  lines = test.split('\n')
+  for i, line in enumerate(lines):
+    line = line.lstrip(' \t').rstrip(' \t\r\n')
+    triple = get_the_3_ytid_filename_pieces_or_nones(line)
+    prename, ytid, dot_ext = triple
+    seq = i + 1
+    scrmsg = f"{seq} | prename=[{prename}] ytid={ytid} dot_ext={dot_ext} [{line}]"
+    print(scrmsg)
+
+
+def process():
+  """
+
+
+  :return:
+  """
+  dirpath = get_args()
+  looker = YtidsInDirLookerUp(dir_abspath=dirpath)
+  looker.process()
+  # looker.list_all_ytids()
+
+
+if __name__ == '__main__':
+  """
+  process()
+  adhoctest1()
+  adhoctest2()
+  """
+  process()
