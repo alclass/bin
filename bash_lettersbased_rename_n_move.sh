4af68ca09e3933ce866e5aea837f7eff1cdd599b
Luiz Lewis <livrosetc@yahoo.com.br>
2026-07-18 19:23:57 -0300

    Updated bash_lettersbased_rename_n_move.sh to use a local .env variable;

--- bash_lettersbased_rename_n_move.sh
+++ bash_lettersbased_rename_n_move.sh
@@ -1,8 +1,14 @@
+# Enable auto-exporting
+set -a  
+# looking up local "$RNER_CLEANSPECSTR" for renameCleanSpecifiedStr.py
+source .env
+# Disable auto-exporting
+set +a
 renamePrefix.py "d' " -e=mp4 -y
 renameAudioDurationIncluder.py -y -e=mp4; renameYtDlpBracketConventionToFormer.py tf -y
 renameCleanBeginning.py -e=mp4 -p=3 -y
 renameCleanRegExp.py -e=mp4 -r="\[\w+\d*\]+" -y
-renameCleanSpecifiedStr.py -e=mp4 -s="RNER.COM - " -y
+renameCleanSpecifiedStr.py -e=mp4 "$ENVCLEANSPECSTR1" -s= -y
 # renameCleanSpecifiedStr.py -e=mp4 -s=" (360)" -y
 renameReplace12.py -e=mp4 -s1="(240)" -s2="240p" -y
 # moveFilesDashedNameToCapSepNamesDir.py
