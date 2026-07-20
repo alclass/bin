20cf350fd1e2d9030662dba710b2a3a8c39b9eb8
Luiz Lewis <livrosetc@yahoo.com.br>
2026-07-19 22:42:54 -0300

    A minor update;

--- cleanUrlEncodedCharsIntoFilenames.py
+++ cleanUrlEncodedCharsIntoFilenames.py
@@ -4,32 +4,34 @@ import os
 
 SPACE_ENC = '%20'
 
-def runCurrentFolder(doRename=False):
-  '''
+def run_current_folder(do_rename= False):
+  """
   runCurrentFolder(doRename=False)
-  '''
+  """
   files = os.listdir('.')
   for eachFile in files:
     if eachFile.find(SPACE_ENC) > -1:
-      newFilename = eachFile.replace(SPACE_ENC, ' ')
-      print '[to rename]', eachFile, newFilename,
-      if doRename:
-        os.rename(eachFile, newFilename)
-        if os.path.isfile(newFilename):
-          print '[renamed]'
+      new_filename = eachFile.replace(SPACE_ENC, ' ')
+      print('[to rename]', eachFile, new_filename,)
+      if do_rename:
+        os.rename(eachFile, new_filename)
+        if os.path.isfile(new_filename):
+          print('[renamed]')
       else: # ie, not renaming, just line feeding former print ending with comma
-        print
+        print()
 
-def processRename():
-  '''
+
+def process_rename():
+  """
   confirm_renames()
-  '''
-  runCurrentFolder(doRename=False)
-  print ' *** QUESTION *** '
-  print 'Rename files above ? (s (or y) / n)'
-  ans = raw_input(' s/n ')
+  """
+  run_current_folder(do_rename=False)
+  print(' *** QUESTION *** ')
+  print('Rename files above ? (s (or y) / n)')
+  ans = input(' s/n ')
   if ans in ['y', 'Y', 's', 'S']:
-    runCurrentFolder(doRename=True)
+    run_current_folder(do_rename=True)
+
 
 if __name__ == '__main__':
-  processRename()
+  process_rename()
